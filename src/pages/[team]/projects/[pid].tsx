4d60acfd2e4ad0668ee511c7a1880e9902d864a3
Anurag Banerjee <anurag@axioned.com>
2023-03-16 23:06:10 +0530

    added the hooks on the necessary routes

--- src/pages/[team]/projects/[pid].tsx
+++ src/pages/[team]/projects/[pid].tsx
@@ -1,17 +1,20 @@
+import Unavailable from "@/components/unavailable";
+import { useValidateTenantAccess } from "@/hooks/tenantValidation";
 import { useSession, getSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Project() {
-  const { data: session, status } = useSession();
   const router = useRouter();
   const { pid } = router.query;
-  if (status === "loading") {
+  const { isLoading, isInvalid } = useValidateTenantAccess();
+
+  if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  if (status === "unauthenticated") {
-    return <p>Access Denied</p>;
+  if (isInvalid) {
+    return <Unavailable />;
   }
   return (
     <div className="mx-auto flex max-w-6xl gap-4">
