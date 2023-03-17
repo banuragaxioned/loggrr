be66222ac4fac3f036ded92047585e1b821d4543
Anurag Banerjee <anurag@axioned.com>
2023-03-17 15:57:00 +0530

    Get members of a tenant

--- src/server/api/routers/tenant.ts
+++ src/server/api/routers/tenant.ts
@@ -17,15 +17,28 @@ export const tenantRouter = createTRPCRouter({
     return tenants;
   }),
   // Get a single Tenant by slug
+  // TODO: Need to move this to the middleware? (see comment on src/server/api/trpc.ts; enforceUserIsAuthorized)
   validateTenantAccess: protectedProcedure
     .input(z.object({ text: z.string() }))
     .query(async ({ ctx, input }) => {
-      // console.log({input.text});
       const slug = input.text;
       const userId = Number(ctx.session.user.id);
       const tenant = await ctx.prisma.tenant.findFirst({
         where: { slug, users: { some: { id: userId } } }, // This works as an OR filter?
       });
       return tenant;
+    }),
+  // Get all members for a Tenant
+  getTenantMembers: protectedProcedure
+    .input(z.object({ text: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const slug = input.text;
+      const userId = Number(ctx.session.user.id);
+      const members = await ctx.prisma.tenant.findFirst({
+        where: { slug, users: { some: { id: userId } } },
+        include: { users: true },
+        // select: { users: { select: { id: true, name: true, image: true } } },
+      });
+      return members;
     }),
 });
