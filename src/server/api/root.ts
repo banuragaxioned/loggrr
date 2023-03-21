9359f21509f259e5b7a9aed939ce03c5cba94395
Anurag Banerjee <anurag@axioned.com>
2023-03-22 00:08:00 +0530

    Remove example, naming convention changes to Prisma relation fields (capitalized)

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { projectRouter } from './routers/project';
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { tenantRouter } from "./routers/tenant";
 
 /**
@@ -9,7 +8,6 @@ import { tenantRouter } from "./routers/tenant";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   tenant: tenantRouter,
   project: projectRouter,
 });
