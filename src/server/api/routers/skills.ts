bd4dbf70c4ce681979ad94a46b99e13f22f48cb2
Anurag Banerjee <anurag@axioned.com>
2023-03-23 18:29:19 +0530

    Update skills.ts

--- src/server/api/routers/skills.ts
+++ src/server/api/routers/skills.ts
@@ -24,6 +24,7 @@ export const skillsRouter = createTRPCRouter({
       const userId = Number(ctx.session.user.id);
       const skillScores = await ctx.prisma.tenant.findFirst({
         where: { slug, Users: { some: { id: userId } } },
+        // TODO: this needs to also filter the resutls for the current user
         include: { SkillScore: true, Skill: true },
       });
       return skillScores;
