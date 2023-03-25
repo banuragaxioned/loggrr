70a971d135fddaf69dc8b7a9083480bd0e29e680
Anurag Banerjee <anurag@axioned.com>
2023-03-25 22:58:04 +0530

    Added base route for timeEntry

--- src/pages/[team]/reports/logged.tsx
+++ src/pages/[team]/reports/logged.tsx
@@ -1,7 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
 import Unavailable from "@/components/unavailable";
 import { useValidateTenantAccess } from "@/hooks/tenantValidation";
+import { api } from "@/utils/api";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
-export default function GlobalReportsLogged() {
+export default function Projects() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const currentTenant = router.query.team as string;
+  const allSkillList = api.timeEntry.getAll.useQuery(
+    { tenant: currentTenant },
+    { enabled: session?.user !== undefined }
+  );
+
   const { isLoading, isInvalid } = useValidateTenantAccess();
 
   if (isLoading) {
@@ -15,7 +27,21 @@ export default function GlobalReportsLogged() {
     <div className="mx-auto flex max-w-6xl gap-4">
       <section>
         <h2>Global Logged Data</h2>
-        <div className="todo h-14">Data table</div>
+        <ul className="flex flex-col gap-4">
+          {allSkillList.data &&
+            allSkillList.data.map((timeEntry) => (
+              <li
+                key={timeEntry.id}
+                className="hover:bg-zinc/20 max-w-none rounded-xl bg-zinc-400/10 p-4 hover:bg-zinc-400/20"
+              >
+                {timeEntry.updatedAt.toLocaleDateString()}-{" "}
+                {timeEntry.Project.Client.name} - {timeEntry.Project.name} -{" "}
+                {timeEntry.time}m{" "}
+                {timeEntry.billable ? <span>🟢</span> : <span>🔴</span>} -{" "}
+                {timeEntry.comments}
+              </li>
+            ))}
+        </ul>
       </section>
     </div>
   );
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { projectRouter } from "./routers/project";
 import { createTRPCRouter } from "./trpc";
 import { tenantRouter } from "./routers/tenant";
 import { skillsRouter } from "./routers/skills";
+import { timeEntryRouter } from "./routers/timeEntry";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   tenant: tenantRouter,
   project: projectRouter,
   skill: skillsRouter,
+  timeEntry: timeEntryRouter
 });
 
 // export type definition of API
