c66c29ccaa1691307fa49a0fe8720f65f9132b57
Anurag Banerjee <anurag@axioned.com>
2023-04-03 17:06:57 +0530

    Create a addMilestone procedure

--- src/server/api/routers/milestone.ts
+++ src/server/api/routers/milestone.ts
@@ -14,4 +14,29 @@ export const milestoneRouter = createTRPCRouter({
       });
       return milestones;
     }),
+  // Create a new Milestone
+  addMilestone: protectedProcedure
+    .input(
+      z.object({
+        slug: z.string(),
+        pid: z.number(),
+        name: z.string(),
+        budget: z.number(),
+        startDate: z.string().datetime().optional(),
+        endDate: z.string().datetime().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+
+      const milestone = await ctx.prisma.milestone.create({
+        data: {
+          Project: { connect: { id: input.pid } },
+          // Need to add a step to check if the name is empty, startDate exists for the milestone and project frequency is not FIXED - in this case, autogenerate the name (create a separate util for this?)
+          name: input.name,
+          budget: input.budget,
+          Tenant: { connect: { slug: input.slug } },
+        },
+      });
+      return milestone;
+    }),
 });
