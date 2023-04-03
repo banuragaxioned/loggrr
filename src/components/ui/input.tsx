024204f8c747ae52068a2ba62dbf3257e434827e
Anurag Banerjee <anurag@axioned.com>
2023-04-03 23:50:50 +0530

    Temp remove to avoid error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { cn } from "@/utils/helper";
-import { useForm } from "react-hook-form";
 import { UseFormRegister } from "react-hook-form";
 
 export interface InputProps
@@ -16,7 +15,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           "flex h-10 w-full rounded-md border border-slate-300 bg-transparent px-3 py-2 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-0 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
           className
         )}
-        {...register(name!)}
+        // {...register(name!)}
         {...props}
         ref={ref}
       />
