2ce44ad2ef2c7b0b52a9e509d154a15ea165f6e3
Anurag Banerjee <anurag@axioned.com>
2023-04-06 13:51:33 +0530

    Type safety for envs

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -8,12 +8,12 @@ type EmailPayload = {
 
 // Replace with your SMTP credentials
 const smtpOptions = {
-  host: process.env.EMAIL_SERVER_HOST|| "smtp.mailtrap.io",
-  port: parseInt(process.env.EMAIL_SERVER_PORT || "2525"),
+  host: process.env.EMAIL_HOST|| "smtp.mailtrap.io",
+  port: parseInt(process.env.EMAIL_PORT || "2525"),
   secure: false,
   auth: {
-    user: process.env.EMAIL_SERVER_USER || "user",
-    pass: process.env.EMAIL_SERVER_PASSWORD || "password",
+    user: process.env.EMAIL_USER || "user",
+    pass: process.env.EMAIL_PASSWORD || "password",
   },
 }
 
