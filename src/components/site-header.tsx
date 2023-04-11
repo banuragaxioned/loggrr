4e9bfdce2203a38d900e4e950c462d12c6e84908
Anurag Banerjee <anurag@axioned.com>
2023-04-11 12:22:57 +0530

    Remove unnecessary wrapper

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -12,35 +12,33 @@ export function SiteHeader() {
   const { data: sessionData } = useSession();
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-b-zinc-200 bg-white px-4 dark:border-b-zinc-700 dark:bg-zinc-950">
-      <div className="container mx-auto flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link href={siteConfig.links.help} target="_blank" rel="noreferrer">
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                  className: "text-zinc-700 dark:text-zinc-400",
-                })}
-              >
-                <Icons.help className="h-5 w-5" />
-                <span className="sr-only">Help</span>
-              </div>
-            </Link>
-            <ThemeToggle />
-            {sessionData && (
-              <UserAccountNav
-                user={{
-                  name: sessionData.user.name,
-                  image: sessionData.user.image,
-                  email: sessionData.user.email,
-                }}
-              />
-            )}
-          </nav>
-        </div>
+    <header className="container sticky top-0 z-40 mx-auto flex h-16 w-full items-center space-x-4 border-b border-b-zinc-200 bg-white px-4 dark:border-b-zinc-700 dark:bg-zinc-950 sm:justify-between sm:space-x-0">
+      <MainNav items={siteConfig.mainNav} />
+      <div className="flex flex-1 items-center justify-end space-x-4">
+        <nav className="flex items-center space-x-1">
+          <Link href={siteConfig.links.help} target="_blank" rel="noreferrer">
+            <div
+              className={buttonVariants({
+                size: "sm",
+                variant: "ghost",
+                className: "text-zinc-700 dark:text-zinc-400",
+              })}
+            >
+              <Icons.help className="h-5 w-5" />
+              <span className="sr-only">Help</span>
+            </div>
+          </Link>
+          <ThemeToggle />
+          {sessionData && (
+            <UserAccountNav
+              user={{
+                name: sessionData.user.name,
+                image: sessionData.user.image,
+                email: sessionData.user.email,
+              }}
+            />
+          )}
+        </nav>
       </div>
     </header>
   );
