6a28397ba5425b697bf8b168dad5a1edc3970967
Anurag Banerjee <anurag@axioned.com>
2023-04-20 20:08:08 +0530

    Add skeleton to the team layout

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/helper";
 
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Skeleton({ className, ...props }: SkeletonProps) {
-  return <div className={cn("h-5 w-2/5 animate-pulse rounded-lg bg-zinc-100", className)} {...props} />;
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("bg-muted animate-pulse rounded-md bg-zinc-400", className)} {...props} />;
 }
+
+export { Skeleton };
