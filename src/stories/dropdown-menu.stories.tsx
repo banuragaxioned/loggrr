da0c2ff992a220408570719e3811127cc5ffe466
kashifaxioned <kashifa@axioned.com>
2023-05-10 20:34:47 +0530

    color pallete updated in dropdown menu component

--- src/stories/dropdown-menu.stories.tsx
+++ src/stories/dropdown-menu.stories.tsx
@@ -24,9 +24,9 @@ export const AdvancedDropdown = () => (
   <div>
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="sm">
-          <Icons.sun className="rotate-0 scale-100 transition-all hover:text-zinc-950 dark:-rotate-90 dark:scale-0 dark:text-zinc-400 dark:hover:text-zinc-100" />
-          <Icons.moon className="absolute rotate-90 scale-0 transition-all hover:text-zinc-950 dark:rotate-0 dark:scale-100 dark:text-zinc-400 dark:hover:text-zinc-100" />
+        <Button variant="primary" size="sm">
+          <Icons.sun className="rotate-0 scale-100 transition-all text-popover-foreground hover:accent hover:text-accent-foreground dark:-rotate-90 dark:scale-0" />
+          <Icons.moon className="absolute rotate-90 scale-0 transition-all text-popover-foreground hover:accent hover:text-accent-foreground dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
