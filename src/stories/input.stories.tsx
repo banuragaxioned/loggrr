a12ffce80f786aa1db9e5d74758b4b744741f4b8
kashifaxioned <kashifa@axioned.com>
2023-05-12 16:01:59 +0530

    decrese width of input component and border color added for popover component

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -2,6 +2,6 @@ import { Input } from "@/components/ui/input";
 
 export const InputBox = () => (
   <div>
-    <Input placeholder="Write some text" className="w-1/2"/>
+    <Input placeholder="Write some text" className="w-1/4"/>
   </div>
 );
--- src/stories/popover.stories.tsx
+++ src/stories/popover.stories.tsx
@@ -9,7 +9,7 @@ export const PopOver = () => {
   return (
     <Popover>
       <PopoverTrigger><Button variant="primary">Open</Button></PopoverTrigger>
-      <PopoverContent side="bottom" align="start">Place content for the popover here.</PopoverContent>
+      <PopoverContent side="bottom" align="start" className="border-border">Place content for the popover here.</PopoverContent>
     </Popover>
 
   )
