2cae47575a97d3d4c2752b5a0b11b1af20262d48
Kashif Ansari <100692718+kashifaxioned@users.noreply.github.com>
2023-05-24 13:14:15 +0530

    Fancy Box (#118)
    * fancy box component created
    
    * spacing added according to combobox
    
    * passing values from where components is used
    
    ---------
    
    Co-authored-by: Anurag Banerjee <anurag@axioned.com>

---
+++ src/components/ui/fancybox.tsx
@@ -0,0 +1,157 @@
+import * as React from "react";
+import { Check, ChevronsUpDown, List, SearchIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type List = Record<"value" | "label", string>;
+
+interface FancyBoxProps {
+  options: List[],
+  selectedValues: List[],
+  setSelectedValues: React.Dispatch<React.SetStateAction<List[]>>
+}
+
+export function FancyBox({options, selectedValues, setSelectedValues}: FancyBoxProps) {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [list, setList] = React.useState<List[]>(options);
+  const [openCombobox, setOpenCombobox] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState<string>("");
+
+  const toggleList = (list: List) => {
+    setSelectedValues((prev) =>
+      !prev.includes(list)
+        ? [...prev, list]
+        : prev.filter((l) => l.value !== list.value)
+    );
+    inputRef?.current?.focus();
+  };
+
+  const createListItem = (name: string) => {
+    const newList = {
+      value: name.toLowerCase(),
+      label: name,
+    };
+    setList((prev) => [...prev, newList]);
+    setSelectedValues((prev) => [...prev, newList]);
+  };
+
+  const onComboboxOpenChange = (value: boolean) => {
+    inputRef.current?.blur();
+    setOpenCombobox(value);
+  };
+
+  return (
+    <div className="max-w-[200px]">
+      <Popover open={openCombobox} onOpenChange={onComboboxOpenChange}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={openCombobox}
+            className="w-[200px] justify-between text-foreground"
+          >
+            <span className="truncate">
+              {selectedValues.length === 0 && "Select labels"}
+              {selectedValues.length === 1 && selectedValues[0].label}
+              {selectedValues.length > 1 && `${selectedValues[0].label} + ${selectedValues.length - 1} more`}
+            </span>
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          side="bottom"
+          align="start"
+          className="max-w-[230px] border-0 bg-popover p-0 text-popover-foreground transition-all ease-in"
+        >
+          <Command loop className="border border-box rounded-t-md border-border">
+            <div className="space-between flex w-full items-center rounded-t-md">
+              <SearchIcon className="h-[14px] shrink-0 basis-[15%] stroke-2" />
+              <CommandInput
+                ref={inputRef}
+                placeholder="Search list..."
+                value={inputValue}
+                onValueChange={setInputValue}
+                className={`text-popover-foreground border-0 box-border rounded-t-sm bg-popover px-0 text-sm leading-6 placeholder:leading-6 placeholder:text-sm placeholder:opacity-75 focus:outline-0 focus:ring-0 overflow-hidden`}
+              />
+            </div>
+            <CommandList
+              className={`border-x border-b border-border scrollbar border-box ComboBox-scrollbar absolute left-1/2 top-full max-h-[240px] w-full -translate-x-1/2 overflow-y-auto rounded-b-[5px] bg-popover px-[5px] py-[8px] shadow-md transition-all duration-200 ease-out`}
+            >
+            <CommandEmpty className="px-[14px] py-2 text-[14px]">No results found.</CommandEmpty>
+            <CommandGroup className="scrollbar ComboBox-scrollbar overflow-auto select-none px-0 text-sm">
+              {list.map((list) => {
+                const isActive = selectedValues.includes(list);
+                return (
+                  <CommandItem
+                    key={list.value}
+                    value={list.value}
+                    onSelect={() => toggleList(list)}
+                    className="w-full flex justify-between cursor-pointer rounded px-[18px] py-2 text-sm aria-selected:bg-hover"
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        isActive ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    <div className="flex-1">{list.label}</div>
+                  </CommandItem>
+                );
+              })}
+               <CommandItemCreate
+                onSelect={() => createListItem(inputValue)}
+                {...{ inputValue, list }}
+              />
+            </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
+
+
+const CommandItemCreate = ({
+  inputValue,
+  list,
+  onSelect,
+}: {
+  inputValue: string;
+  list: List[];
+  onSelect: () => void;
+}) => {
+  const hasNoList = !list
+    .map(({ value }) => value)
+    .includes(`${inputValue.toLowerCase()}`);
+
+  const render = inputValue !== "" && hasNoList;
+
+  if (!render) return null;
+
+  // BUG: whenever a space is appended, the Create-Button will not be shown.
+  return (
+    <CommandItem
+      key={`${inputValue}`}
+      value={`${inputValue}`}
+      className="w-full cursor-pointer px-[18px] py-2 text-sm text-muted-foreground aria-selected:bg-hover rounded-md"
+      onSelect={onSelect}
+    >
+      Create new label &quot;{inputValue}&quot;
+    </CommandItem>
+  );
+};
---
+++ src/stories/fancybox.stories.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react"
+import { FancyBox } from "@/components/ui/fancybox";
+
+type List = Record<"value" | "label", string>;
+
+const options = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+  {
+    value: "wordpress",
+    label: "WordPress",
+  },
+]satisfies List[];
+
+export const FancyBoxDemo = () => {
+  const [selectedValues, setSelectedValues] = React.useState<List[]>([]);
+
+  return (
+  <div>
+    <FancyBox options={options} selectedValues={selectedValues} setSelectedValues={setSelectedValues}/>
+  </div>
+  )
+}
+
