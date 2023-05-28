60e1715e21e598cea81fd4746a6e75d81c68c9ff
Anurag Banerjee <anurag@axioned.com>
2023-05-29 00:52:24 +0530

    Fix up client listing page

--- src/app/launchpad/[team]/(team)/clients/page.tsx
+++ src/app/launchpad/[team]/(team)/clients/page.tsx
@@ -3,19 +3,7 @@ import { DashboardHeader } from "@/components/ui/header";
 import { DashboardShell } from "@/components/ui/shell";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { type Tenant } from "@prisma/client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { NewClientForm } from "@/components/clientForm";
 
 export default async function Page({ params }: { params: { team: Tenant["slug"] } }) {
   const { team } = params;
@@ -23,39 +11,7 @@ export default async function Page({ params }: { params: { team: Tenant["slug"] 
   return (
     <DashboardShell>
       <DashboardHeader heading="Clients" text="This is a list of all clients">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline">Add</Button>
-          </SheetTrigger>
-          <SheetContent position="right" size="sm">
-            <SheetHeader>
-              <SheetTitle>New client</SheetTitle>
-              <SheetDescription>
-                Enter the name of the new client. This has to be unique. Make it easier for your team to fine and
-                recognize.
-              </SheetDescription>
-            </SheetHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input id="name" value="Pedro Duarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-            </div>
-            <SheetFooter>
-              <SheetClose asChild>
-                <Button type="submit">Create</Button>
-              </SheetClose>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
+        <NewClientForm team={team} />
       </DashboardHeader>
       <Table>
         <TableHeader>
