d0854d4a933135074217a42f2e4861ce5fa9f471
prdxn88 <omkarv@axioned.com>
2023-06-20 12:49:51 +0530

    fixed build issue

--- src/stories/fancybox.stories.tsx
+++ src/stories/fancybox.stories.tsx
@@ -35,7 +35,7 @@ export const FancyBoxDemo = () => {
 
   return (
     <div>
-      <FancyBox options={options} selectedValues={selectedValues} setSelectedValues={setSelectedValues} />
+      <FancyBox options={options} selectedValues={selectedValues} setSelectedValues={setSelectedValues} defaultLabel="Select Labels"/>
     </div>
   );
 };
