94b2362fb91edc97924096eba24e78f152795263
Anurag Banerjee <anurag@axioned.com>
2023-06-26 12:13:23 +0530

    Add team switcher to site-header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,13 +7,17 @@ import { ThemeToggle } from "@/components/theme-toggle";
 import { buttonVariants } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 import { UserAccountNav } from "@/components/user-account";
+import TeamSwitcher from "@/components/teamSwitcher";
 
 export function SiteHeader() {
   const { data: sessionData } = useSession();
+  const teamData = sessionData?.user.tenants;
+  console.log(teamData);
 
   return (
     <header className="container sticky top-0 z-40 mx-auto flex h-16 w-full items-center space-x-4 border-b border-b-zinc-200 bg-white px-4 dark:border-b-zinc-700 dark:bg-zinc-950 sm:justify-between sm:space-x-0">
       <MainNav items={siteConfig.mainNav} />
+      {sessionData && <TeamSwitcher team={team} />}
       <div className="flex flex-1 items-center justify-end space-x-4">
         <nav className="flex items-center space-x-1">
           <Link href={siteConfig.links.help} target="_blank" rel="noreferrer">
