3f1a18502372ab6a966750dd670d2002c02c7ef8
Anurag Banerjee <anurag@axioned.com>
2023-06-27 01:02:25 +0530

    Remove console.log

--- src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
+++ src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
@@ -24,8 +24,6 @@ export default async function Assigned({ params }: { params: { team: Tenant["slu
 
   const allocation = await getAllocations(options);
 
-  console.log(allocation);
-
   return (
     <>
       <DashboardShell>
