f67872efc779631cad3d5873f7bd2346942bf283
Anurag Banerjee <anurag@axioned.com>
2023-06-28 22:42:26 +0530

    Add Scatterchart

--- src/app/launchpad/[team]/(skills)/skills/report/page.tsx
+++ src/app/launchpad/[team]/(skills)/skills/report/page.tsx
@@ -1,7 +1,10 @@
+import ScatterDashboard from "@/components/skillWidget";
+
 export default async function Page() {
   return (
     <>
       <h3>Coming soon</h3>
+      <ScatterDashboard />
     </>
   );
 }
