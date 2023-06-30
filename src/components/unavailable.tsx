1a688f76646bf5c0d83a87ea4c7749aa386c0d05
Anurag Banerjee <anurag@axioned.com>
2023-06-30 15:55:17 +0530

    Add not-found route

--- src/components/unavailable.tsx
+++ src/components/unavailable.tsx
@@ -3,22 +3,20 @@ import { buttonVariants } from "@/components/ui/button";
 
 export default function Unavailable() {
   return (
-    <div className="mx-auto flex max-w-6xl gap-4">
-      <section>
-        <h2>Page unavailable</h2>
-        <p>
-          The link you are trying to reach is currently unavailable. Please check if you are using the correct link. If
-          your page has not appeared again in 5-10 minutes then please contact our support team.
-        </p>
-        <div className="flex gap-4">
-          <Link href="/help" rel="noreferrer" className={buttonVariants({ size: "lg" })}>
-            Contact
-          </Link>
-          <Link rel="noreferrer" href="/" className={buttonVariants({ variant: "primary", size: "lg" })}>
-            Back to home
-          </Link>
-        </div>
-      </section>
-    </div>
+    <section>
+      <h1>Page unavailable</h1>
+      <p>
+        The link you are trying to reach is currently unavailable. Please check if you are using the correct link. If
+        your page has not appeared again in 5-10 minutes then please contact our support team.
+      </p>
+      <div className="mt-2 flex gap-4">
+        <Link href="mailto:loggr@axioned.com" rel="noreferrer" className={buttonVariants({ size: "lg" })}>
+          Contact
+        </Link>
+        <Link rel="noreferrer" href="/" className={buttonVariants({ variant: "primary", size: "lg" })}>
+          Back to home
+        </Link>
+      </div>
+    </section>
   );
 }
