f32de2653084131201d8c64956941b9c10f6dc36
Anurag Banerjee <anurag@axioned.com>
2023-07-03 18:46:53 +0530

    Reset report

--- src/app/launchpad/[team]/(skills)/skills/report/page.tsx
+++ src/app/launchpad/[team]/(skills)/skills/report/page.tsx
@@ -1,10 +1,7 @@
-import ScatterDashboard from "@/components/skillWidget";
-
 export default async function Page() {
   return (
     <>
       <h3>Coming soon</h3>
-      <ScatterDashboard />
     </>
   );
 }
