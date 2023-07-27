257d67780191e1a13f58357a505d5f7de4e7d829
kashifaxioned <kashifa@axioned.com>
2023-07-27 19:20:38 +0530

    route created for update allocation

---
+++ src/app/api/team/allocation/update/route.ts
@@ -0,0 +1,60 @@
+import { getServerSession } from "next-auth/next";
+import * as z from "zod";
+import { authOptions } from "@/server/auth";
+import { db } from "@/lib/db";
+import { AllocationFrequency } from "@prisma/client";
+
+const allocationUpdateSchema = z.object({
+  team: z.string().min(1),
+  allocationId: z.coerce.number().min(1),
+  date: z.coerce.date(),
+  frequency: z.nativeEnum(AllocationFrequency),
+  enddate: z.coerce.date().optional(),
+  billableTime: z.coerce.number(),
+  nonBillableTime: z.coerce.number(),
+});
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const { user } = session;
+
+    const json = await req.json();
+    const body = allocationUpdateSchema.parse(json);
+
+    // check if the user has permission to the current team/tenant id if not return 403
+    // user session has an object (name, id, slug, etc) of all tenants the user has access to. i want to match slug.
+    if (user.tenants.filter((tenant) => tenant.slug === body.team).length === 0) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const client = await db.allocation.update({
+      where: {
+        Tenant: {
+          slug: body.team,
+        },
+        id: body.allocationId,
+      },
+      data: {
+        date: body.date,
+        enddate: body.enddate,
+        frequency: body.frequency,
+        billableTime: body.billableTime,
+        nonBillableTime: body.nonBillableTime,
+      },
+    });
+
+    return new Response(JSON.stringify(client));
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return new Response(null, { status: 500 });
+  }
+}
