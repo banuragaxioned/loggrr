7de818cb9e17d21f28e838564a5321d99bb1f07c
prdxn88 <omkarv@axioned.com>
2023-08-04 16:37:48 +0530

    removed fallback

--- src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
+++ src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
@@ -4,21 +4,17 @@ import { DashboardHeader } from "@/components/ui/header";
 import { Tenant } from "@prisma/client";
 import { getProjectsId, getAllUsers } from "@/server/services/allocation";
 import { NewAllocationForm } from "@/components/forms/allocationForm";
-import { Suspense, lazy } from "react";
 
 export default async function Assigned({ params }: { params: { team: Tenant["slug"] } }) {
   const { team } = params;
   const projects = await getProjectsId(team);
   const users = await getAllUsers(team);
-  const NewAllocationForm = lazy(():any=>import("@/components/forms/allocationForm"));
 
   return (
     <>
       <DashboardShell>
         <DashboardHeader heading="Assignments" text="This is a summary current assignments">
-          <Suspense fallback={<p>loading</p>}>
           <NewAllocationForm team={team} projects={projects} users={users} />
-          </Suspense>
         </DashboardHeader>
           <DataTable team={team} />
       </DashboardShell>
