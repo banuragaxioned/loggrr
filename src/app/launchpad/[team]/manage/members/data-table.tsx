443008c37ab0f4c52820b4276d39970d3239e995
prdxn88 <omkarv@axioned.com>
2023-08-09 12:40:58 +0530

    added types for data table

--- src/app/launchpad/[team]/manage/members/data-table.tsx
+++ src/app/launchpad/[team]/manage/members/data-table.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Hourglass, FolderPlus } from "lucide-react";
 import { UserAvatar } from "@/components/user-avatar";
 
+import * as React from "react";
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -18,105 +18,65 @@ import {
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { FancyBox, List } from "@/components/ui/fancybox";
-import { Archive, ChevronsUpDown } from "lucide-react";
-
-import * as React from "react";
+import { ChevronsUpDown } from "lucide-react";
+import { Member } from "@/types";
 
 interface DataTableProps<TData, TValue> {
-  data: TData[]|any;
+  data: TData[] | any;
 }
 
-const columns: ColumnDef<any>[] = [
-    {
-      accessorKey: "name",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="link"
-            className="text-slate-500"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Name
-            <ChevronsUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        );
-      },
+const columns: ColumnDef<Member | any>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          className="text-slate-500"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
     },
-    {
-      accessorKey: "mail",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="link"
-            className="text-slate-500"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Email
-            <ChevronsUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        );
-      },
+  },
+  {
+    accessorKey: "mail",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          className="text-slate-500"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
     },
-    {
-      accessorKey: "role",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="link"
-            className="text-slate-500"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Role
-            <ChevronsUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        );
-      },
+  },
+  {
+    accessorKey: "role",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          className="text-slate-500"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Role
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
     },
-  ];
-  
+  },
+];
 
 export function DataTable<TData, TValue>({ data }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
-  const [selectedProjects, setSelectedProjects] = React.useState<List[]>([]);
-  const [selectedClient, setSelectedClient] = React.useState<List[]>([]);
-  const [selectedLead, setSelectedLead] = React.useState<List[]>([]);
-
-  const dataFormator = (arr: never[], key: string) => {
-    return arr.map((obj: any) => ({ label: obj[key], value: obj[key] }));
-  };
-
-  const dataFilter = (arr: [] | any, key: string) => {
-    const formatedArr = dataFormator(arr, key);
-    const processedData = formatedArr.filter((obj: any, i: number) => {
-      const repeated = formatedArr.slice(i + 1, arr.length).find((item: any) => item?.label === obj?.label);
-      if (!repeated && obj?.label) {
-        return obj;
-      }
-    });
-    return processedData;
-  };
-
-  const filterMatcher = (rowObj: any) => {
-    let check = true;
-    const projectName = rowObj.original.name;
-    const lead = rowObj.original.projectOwner;
-    const client = rowObj.original.clientName;
-    const selectedReference = [
-      selectedProjects.length && selectedProjects,
-      selectedClient.length && selectedClient,
-      selectedLead.length && selectedLead,
-    ];
-    selectedReference.map((item, i) => {
-      if (item) {
-        check =
-          check && item.find((obj) => obj.value === (i === 2 ? lead : i === 1 ? client : projectName)) !== undefined;
-      }
-    });
-    return check;
-  };
 
   const table = useReactTable({
     data,
@@ -157,23 +117,18 @@ export function DataTable<TData, TValue>({ data }: DataTableProps<TData, TValue>
               <TableRow key={row.id} data-state={row.getIsSelected() && "selected"} className="group">
                 {row.getVisibleCells().map((cell: any, j: number) => {
                   return (
-                    (filterMatcher(cell.row) ||
-                      (!selectedProjects.length && !selectedClient.length && !selectedLead.length)) && (
-                      <TableCell
-                        className={`h-[43px] max-h-[43px] px-8 py-0 tabular-nums ${
-                          j <1 ? "flex items-center" : ""
-                        }`}
-                        key={cell.id}
-                      >
-                        {j < 1 && (
-                          <UserAvatar
-                            user={{ name: cell.row.original.name, image: cell.row.original.avatar }}
-                            className="mr-2 inline-block h-5 w-5"
-                          />
-                        )}
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
-                    )
+                    <TableCell
+                      className={`h-[43px] max-h-[43px] px-8 py-0 tabular-nums ${j < 1 ? "flex items-center" : ""}`}
+                      key={cell.id}
+                    >
+                      {j < 1 && (
+                        <UserAvatar
+                          user={{ name: cell.row.original.name, image: cell.row.original.avatar }}
+                          className="mr-2 inline-block h-5 w-5"
+                        />
+                      )}
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
                   );
                 })}
               </TableRow>
--- src/server/services/members.ts
+++ src/server/services/members.ts
@@ -1,35 +1,32 @@
-import { Tenant, User } from "@prisma/client";
-import { prisma } from "../db";
 import { db } from "@/lib/db";
 
+export const getMembers = async (team: string) => {
+  const roles = await db.tenant.findUnique({
+    where: { slug: team },
+    select: {
+      UserRole: {
+        select: {
+          id: true,
+          userId: true,
+          role: true,
+          User: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+              email: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
-export const getMembers = async (team:string)=> {
-    const roles = await db.tenant.findUnique({
-        where:{slug:team},
-        select:{
-           UserRole:{
-            select:{
-                id:true,
-                userId:true,
-                role:true,
-                User:{
-                    select:{
-                        id:true,
-                        name:true,
-                        image:true,
-                        email:true
-                    }
-                }
-            }
-           }
-        }
-    });
-
-    const members = await roles?.UserRole.map( (obj)=> {
-        const id = obj.id;
-        const role = obj.role;
-        const user =  obj.User;
-        return {id,userId:user?.id,name:user?.name,avatar:user?.image,mail:user?.email,role}; 
-    })
-    return members;
-}
+  const members = await roles?.UserRole.map((obj) => {
+    const id = obj.id;
+    const role = obj.role;
+    const user = obj.User;
+    return { id, userId: user?.id, name: user?.name, avatar: user?.image, mail: user?.email, role };
+  });
+  return members;
+};
