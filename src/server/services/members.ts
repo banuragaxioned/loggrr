0b166d31d22e1d6c35ef4be0d10d8250718b2361
Anurag Banerjee <anurag@axioned.com>
2023-08-11 00:51:42 +0530

    Reverted to service

---
+++ src/server/services/members.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { Role, Status } from "@prisma/client";
+
+export const getMembers = async (team: string) => {
+  const data = await db.tenant.findUnique({
+    where: { slug: team },
+    select: {
+      Users: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          image: true,
+          status: true,
+          Roles: {
+            select: {
+              role: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const members = data?.Users.map((member) => {
+    return {
+      id: member.id,
+      name: member.name,
+      email: member.email,
+      image: member.image,
+      status: member.status as Status,
+      role: member.Roles.map((userRole) => userRole.role)[0] as Role,
+    };
+  });
+
+  return members;
+};
