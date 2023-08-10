f71eff87c0a499108fd6d01ad7edd4767c6142a8
Anurag Banerjee <anurag@axioned.com>
2023-08-10 23:13:00 +0530

    Use User instead UserRole as top level query

--- src/app/api/team/members/route.ts
+++ src/app/api/team/members/route.ts
@@ -4,63 +4,64 @@ import { authOptions } from "@/server/auth";
 import * as z from "zod";
 
 const memberSchema = z.object({
-    team: z.string().min(1),
-  });
-  
-export const POST  = async (req : Request) => {
-    try {
-        const session = await getServerSession(authOptions);
-    
-        if (!session) {
-          return new Response("Unauthorized", { status: 403 });
-        }
-    
-        const { user } = session;
-    
-        const json = await req.json();
-        const body = memberSchema.parse(json);
-    
-        // check if the user has permission to the current team/tenant id if not return 403
-        // user session has an object (name, id, slug, etc) of all tenants the user has access to. i want to match slug.
-        if (user.tenants.filter((tenant) => tenant.slug === body.team).length === 0) {
-          return new Response("Unauthorized", { status: 403 });
-        }
-            const roles = await db.tenant.findUnique({
-              where: { slug: body.team },
-              select: {
-                UserRole: {
-                  select: {
-                    id: true,
-                    userId: true,
-                    role: true,
-                    User: {
-                      select: {
-                        id: true,
-                        name: true,
-                        image: true,
-                        email: true,
-                        status:true
-                      },
-                    },
-                  },
-                },
-              },
-            });
-          
-            const members = await roles?.UserRole.map((obj) => {
-              const id = obj.id;
-              const role = obj.role;
-              const user = obj.User;
-              return { id, userId: user?.id, name: user?.name, avatar: user?.image, mail: user?.email, role,status:user.status };
-            });
+  team: z.string().min(1),
+});
 
-        return new Response(JSON.stringify({ members }));
-      } catch (error) {
-        if (error instanceof z.ZodError) {
-          return new Response(JSON.stringify(error.issues), { status: 422 });
-        }
-    
-        return new Response(null, { status: 500 });
-      }
-    
+export const POST = async (req: Request) => {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const { user } = session;
+
+    const json = await req.json();
+    const body = memberSchema.parse(json);
+
+    // check if the user has permission to the current team/tenant id if not return 403
+    // user session has an object (name, id, slug, etc) of all tenants the user has access to. i want to match slug.
+    if (user.tenants.filter((tenant) => tenant.slug === body.team).length === 0) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+    const memberList = await db.tenant.findUnique({
+      where: { slug: body.team },
+      select: {
+        Users: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+            status: true,
+            Roles: {
+              select: {
+                role: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const members = await memberList?.Users.map((member) => {
+      const role = member.Roles[0].role;
+      return {
+        id: user.id,
+        name: user.name,
+        image: user.image,
+        email: user.email,
+        role: role,
+      };
+    });
+
+    return new Response(JSON.stringify({ members }));
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return new Response(null, { status: 500 });
+  }
 };
