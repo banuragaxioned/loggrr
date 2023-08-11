fa993b865a5c7e270acacfb12ce94763d172be85
kashifaxioned <kashifa@axioned.com>
2023-08-11 18:32:31 +0530

    create: reusable skill score select component

---
+++ src/components/selectSkill.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Ban, Brain, Circle, CircleDashed, CircleDot, CircleDotDashed, LucideIcon } from "lucide-react";
+
+type Scores = {
+  id: number;
+  name: string;
+  level: number;
+};
+
+type Level = {
+  id: number;
+  name: string;
+  icon: LucideIcon;
+};
+
+const levels: Level[] = [
+  {
+    id: 0,
+    name: "NA",
+    icon: Ban,
+  },
+  {
+    id: 1,
+    name: "Beginner",
+    icon: CircleDashed,
+  },
+  {
+    id: 2,
+    name: "Intermediate",
+    icon: Circle,
+  },
+  {
+    id: 3,
+    name: "Senior",
+    icon: CircleDotDashed,
+  },
+  {
+    id: 4,
+    name: "Lead",
+    icon: CircleDot,
+  },
+  {
+    id: 5,
+    name: "Expert",
+    icon: Brain,
+  },
+];
+
+export function SelectSkillLevel({ skill, setValue }: { skill: Scores; setValue: (skill: number, value: string) => void}) {
+  return (
+    <Select defaultValue={String(skill.level)} onValueChange={(value) => setValue(skill.id, value)}>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Select" />
+      </SelectTrigger>
+      <SelectContent>
+        {levels.map((level) => (
+          <SelectItem key={level.id} value={String(level.id)}>
+            <div className="flex items-center space-x-4">
+              <div>
+                <level.icon size={16} />
+              </div>
+              <div>
+                <p className="text-sm font-medium leading-none">{level.name}</p>
+              </div>
+            </div>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
