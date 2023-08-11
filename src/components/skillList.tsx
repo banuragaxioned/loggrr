e69fcd182ead14a8c8c7b4c3c99186f4a40678d4
kashifaxioned <kashifa@axioned.com>
2023-08-11 18:38:02 +0530

    skill score select component used

--- src/components/skillList.tsx
+++ src/components/skillList.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Ban, Brain, Circle, CircleDashed, CircleDot, CircleDotDashed, LucideIcon } from "lucide-react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Tenant } from "@prisma/client";
 import useToast from "@/hooks/useToast";
 import { useRouter } from "next/navigation";
+import { SelectSkillLevel } from "./selectSkill";
 
 type Scores = {
   id: number;
@@ -13,47 +12,8 @@ type Scores = {
   level: number;
 }[];
 
-type Level = {
-  id: number;
-  name: string;
-  icon: LucideIcon;
-};
-
-const levels: Level[] = [
-  {
-    id: 0,
-    name: "NA",
-    icon: Ban,
-  },
-  {
-    id: 1,
-    name: "Beginner",
-    icon: CircleDashed,
-  },
-  {
-    id: 2,
-    name: "Intermediate",
-    icon: Circle,
-  },
-  {
-    id: 3,
-    name: "Senior",
-    icon: CircleDotDashed,
-  },
-  {
-    id: 4,
-    name: "Lead",
-    icon: CircleDot,
-  },
-  {
-    id: 5,
-    name: "Expert",
-    icon: Brain,
-  },
-];
-
-export function SkillList({ props, currentUser, team }: { props: Scores, currentUser: number, team: Tenant["slug"] }) {
-  const router = useRouter()
+export function SkillList({ props, currentUser, team }: { props: Scores; currentUser: number; team: Tenant["slug"] }) {
+  const router = useRouter();
   const showToast = useToast();
 
   async function Update(skill: number, value: string) {
@@ -66,13 +26,13 @@ export function SkillList({ props, currentUser, team }: { props: Scores, current
         team: team,
         userId: currentUser,
         skillId: skill,
-        level: Number(value)
+        level: Number(value),
       }),
     });
 
-    if(response?.ok) showToast('Skill updated', 'success');
+    if (response?.ok) showToast("Skill updated", "success");
 
-    router.refresh()
+    router.refresh();
   }
 
   return (
@@ -90,25 +50,9 @@ export function SkillList({ props, currentUser, team }: { props: Scores, current
                   <p className="text-sm font-medium leading-none">{skill.name}</p>
                 </div>
               </div>
-              <Select defaultValue={String(skill.level)} onValueChange={(value) => Update(skill.id, value)}>
-                <SelectTrigger className="ml-auto w-[200px]">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent>
-                  {levels.map((level) => (
-                    <SelectItem key={level.id} value={String(level.id)}>
-                      <div className="flex items-center space-x-4">
-                        <div>
-                          <level.icon size={16} />
-                        </div>
-                        <div>
-                          <p className="text-sm font-medium leading-none">{level.name}</p>
-                        </div>
-                      </div>
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              <div className="ml-auto w-[200px]">
+                <SelectSkillLevel skill={skill} setValue={Update} />
+              </div>
             </div>
           ))}
         </div>
