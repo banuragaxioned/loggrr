25cf3c3622e68cdc4ad927a807e38e7062fddaca
kashifaxioned <kashifa@axioned.com>
2023-08-11 19:10:10 +0530

    addskill form added

--- src/app/launchpad/[team]/(skills)/skills/summary/page.tsx
+++ src/app/launchpad/[team]/(skills)/skills/summary/page.tsx
@@ -4,9 +4,11 @@ import { Tenant } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { DashboardHeader } from "@/components/ui/header";
 import { DashboardShell } from "@/components/ui/shell";
-import { getUserSkills } from "@/server/services/skill";
+import { getSkills, getUserSkills } from "@/server/services/skill";
 import { Overview } from "@/components/skillWidget";
 import { SkillList } from "@/components/skillList";
+import { getAllUsers } from "@/server/services/allocation";
+import { AddSKill } from "@/components/forms/addSkillForm";
 
 type Scores = {
   id: number;
@@ -16,6 +18,8 @@ type Scores = {
 
 export default async function SkillsSummary({ params }: { params: { team: Tenant["slug"] } }) {
   const user = await getCurrentUser();
+  const users = await getAllUsers(params.team);
+  const skillsList = await getSkills(params.team);
 
   if (!user) {
     redirect(authOptions?.pages?.signIn || "/login");
@@ -27,7 +31,9 @@ export default async function SkillsSummary({ params }: { params: { team: Tenant
       <DashboardHeader
         heading="My Skills"
         text="This is a summary of your skills that you have been assessed on."
-      ></DashboardHeader>
+      >
+        <AddSKill team={params.team} users={users} currentUser={user.id} skillsList={skillsList} />
+      </DashboardHeader>
       <Overview data={skills} />
       <SkillList props={skills} currentUser={user.id} team={params.team}/>
     </DashboardShell>
