1fb5576ec5dfc33e572edc1f35e4078ec13ec042
Anurag Banerjee <anurag@axioned.com>
2023-08-14 12:14:42 +0530

    Add config for filter values and icons

---
+++ src/config/filters.ts
@@ -0,0 +1,32 @@
+import { Icons } from "@/components/icons";
+
+export const statuses = [
+  {
+    value: "ARCHIVE",
+    label: "Archived",
+    icon: Icons.archive,
+  },
+  {
+    value: "ACTIVE",
+    label: "Active",
+    icon: Icons.activity,
+  },
+];
+
+export const roles = [
+  {
+    value: "OWNER",
+    label: "Owner",
+    icon: Icons.archive,
+  },
+  {
+    value: "MANAGER",
+    label: "Manager",
+    icon: Icons.activity,
+  },
+  {
+    value: "USER",
+    label: "User",
+    icon: Icons.user,
+  },
+];
