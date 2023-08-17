737ced8a0e0433784aee5f9cbd657b35fd0b3d86
kashifaxioned <kashifa@axioned.com>
2023-08-17 22:16:28 +0530

    update skillscore config file

--- src/config/skillScore.ts
+++ src/config/skillScore.ts
@@ -2,6 +2,7 @@ import { Ban, Brain, Circle, CircleDashed, CircleDot, CircleDotDashed, LucideIco
 
 type Level = {
   id: number;
+  value: number;
   name: string;
   icon: LucideIcon;
 };
@@ -9,31 +10,37 @@ type Level = {
 export const levels: Level[] = [
   {
     id: 0,
+    value: 0,
     name: "NA",
     icon: Ban,
   },
   {
     id: 1,
+    value: 1,
     name: "Beginner",
     icon: CircleDashed,
   },
   {
     id: 2,
+    value: 2,
     name: "Intermediate",
     icon: Circle,
   },
   {
     id: 3,
+    value: 3,
     name: "Senior",
     icon: CircleDotDashed,
   },
   {
     id: 4,
+    value: 4,
     name: "Lead",
     icon: CircleDot,
   },
   {
     id: 5,
+    value: 5,
     name: "Expert",
     icon: Brain,
   },
