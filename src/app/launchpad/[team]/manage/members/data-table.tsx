f9d23bcfad029ce7de49e3b18de211ecc6609c15
Anurag Banerjee <anurag@axioned.com>
2023-08-18 21:42:05 +0530

    Use cn util instead of additional const

--- src/app/launchpad/[team]/manage/members/data-table.tsx
+++ src/app/launchpad/[team]/manage/members/data-table.tsx
@@ -18,6 +18,7 @@ import {
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { DataTablePagination } from "@/components/data-table-pagination";
 import { DataTableToolbar } from "./toolbar";
+import { cn } from "@/lib/utils";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -60,9 +61,8 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
-                const style = header.column.columnDef.meta?.className;
                 return (
-                  <TableHead key={header.id} className={style ? style : ""}>
+                  <TableHead key={header.id} className={cn(header.column.columnDef.meta?.className)}>
                     {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
                   </TableHead>
                 );
@@ -75,9 +75,8 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
             table.getRowModel().rows.map((row) => (
               <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
                 {row.getVisibleCells().map((cell) => {
-                  const style = cell.column.columnDef.meta?.className;
                   return (
-                    <TableCell key={cell.id} className={style ? style : ""}>
+                    <TableCell key={cell.id} className={cn(cell.column.columnDef.meta?.className)}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   );
