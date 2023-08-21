919f1e926745476f908e22b53cc29aa4ca9ae5ef
Omkar Valve <omkarv@axioned.com>
2023-08-21 15:51:24 +0530

    Added: Data table in projects page

---
+++ src/app/launchpad/[team]/(team)/projects/columns.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/data-table-column-header";
+import { Status } from "@prisma/client";
+
+export type Projects = {
+  id: number;
+  name: string | null | undefined;
+  status: Status;
+};
+
+export const columns: ColumnDef<Projects>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Name" />,
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Status" />,
+    filterFn: "arrIncludesSome",
+  },
+];
