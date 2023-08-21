04fb89f7a3a3351250cc206d11482d04586335c8
Omkar Valve <omkarv@axioned.com>
2023-08-21 15:18:43 +0530

    Deleted: unwanted react table in clients page

--- src/app/launchpad/[team]/(team)/clients/table.tsx
+++ src/app/launchpad/[team]/(team)/clients/table.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { DataTable } from "@/components/data-table";
 import { TableProps } from "@/types";
-import { SortingState, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import { SortingState, getSortedRowModel } from "@tanstack/react-table";
 
 export function Table<TData, TValue>({ columns, data }: TableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
