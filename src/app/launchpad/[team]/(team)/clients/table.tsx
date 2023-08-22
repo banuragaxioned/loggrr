dd24b5711504666de6c85cb303432cfb146f0b5d
Anurag Banerjee <anurag@axioned.com>
2023-08-22 13:16:32 +0530

    Format

--- src/app/launchpad/[team]/(team)/clients/table.tsx
+++ src/app/launchpad/[team]/(team)/clients/table.tsx
@@ -1,12 +1,15 @@
 "use client";
+
 import * as React from "react";
 import { DataTableStructure } from "@/components/data-table-structure";
 import { TableProps } from "@/types";
-import {   ColumnFiltersState,
+import {
+  ColumnFiltersState,
   SortingState,
   getCoreRowModel,
   getFilteredRowModel,
-  getSortedRowModel,} from "@tanstack/react-table";
+  getSortedRowModel,
+} from "@tanstack/react-table";
 import { DataTableToolbar } from "./toolbar";
 
 export function Table<TData, TValue>({ columns, data }: TableProps<TData, TValue>) {
@@ -27,5 +30,5 @@ export function Table<TData, TValue>({ columns, data }: TableProps<TData, TValue
     getSortedRowModel: getSortedRowModel(),
   };
 
-  return <DataTableStructure tableConfig={tableConfig} DataTableToolbar={DataTableToolbar}/>;
+  return <DataTableStructure tableConfig={tableConfig} DataTableToolbar={DataTableToolbar} />;
 }
--- src/config/filters.ts
+++ src/config/filters.ts
@@ -47,4 +47,4 @@ export const clientStatuses = [
     label: "Archived",
     icon: Icons.archive,
   },
-]
+];
