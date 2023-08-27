354c818a6beb6d222ded53622b9196ae4453aec9
Anurag Banerjee <anurag@axioned.com>
2023-08-27 10:35:33 +0530

    Add persist and use composable pattern

--- src/store/useGlobalStore.ts
+++ src/store/useGlobalStore.ts
@@ -1,21 +1,42 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
+// Define the interface of the Global state
 interface GlobalState {
-  showAll: boolean; // Show all projects and clients lists
   showArchived: boolean; // Show archived items
-  toggleShowAll: () => void;
-  toggleArchived: () => void;
 }
 
-export const useGlobalStore = create<GlobalState>((set) => ({
-  showAll: false,
+// Define the interface of the actions that can be performed
+interface GlobalStateActions {
+  toggleArchived: () => void;
+  reset: () => void;
+}
+
+// Initialize a default state
+const INITIAL_STATE: GlobalState = {
   showArchived: false,
-  toggleShowAll: () =>
-    set((state) => ({
-      showAll: !state.showAll,
-    })),
-  toggleArchived: () =>
-    set((state) => ({
-      showArchived: !state.showArchived,
-    })),
-}));
+};
+
+// Create the store with Zustand, combining the status interface and actions
+const createGlobalState = (key: string) => {
+  return create<GlobalState & GlobalStateActions>()(
+    persist(
+      (set) => ({
+        ...INITIAL_STATE,
+        toggleArchived() {
+          set((state) => ({
+            showArchived: !state.showArchived,
+          }));
+        },
+        reset() {
+          set(INITIAL_STATE);
+        },
+      }),
+      {
+        name: key,
+      },
+    ),
+  );
+};
+
+export const useGlobalState = createGlobalState("global-state");
