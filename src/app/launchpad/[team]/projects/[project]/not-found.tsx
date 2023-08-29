56fc041141d905cd5a71f25aa20cb5cf132e3773
dshirlyaxioned <shirlyd@axioned.com>
2023-08-29 17:42:44 +0530

    feat: LOG-426 wip

---
+++ src/app/launchpad/[team]/projects/[project]/not-found.tsx
@@ -0,0 +1,6 @@
+import Unavailable from "@/components/unavailable";
+import Link from "next/link";
+
+export default function NotFound() {
+  return <Unavailable />;
+}
--- src/app/launchpad/[team]/projects/[project]/page.tsx
+++ src/app/launchpad/[team]/projects/[project]/page.tsx
@@ -1,8 +1,35 @@
 import { DashboardHeader } from "@/components/ui/header";
 import { DashboardShell } from "@/components/ui/shell";
-import { pageProps } from "@/types";
+import { getCurrentUser } from "@/lib/session";
+import { prisma } from "@/server/db";
+import { notFound } from "next/navigation";
+import { pageProps, projectProps } from "@/types";
 
-export default async function Page({ params }: pageProps) {
+export default async function Page({ params }: projectProps) {
+  const user = await getCurrentUser();
+  const { team, project } = params;
+
+  console.log(params, project);
+  
+
+  if (!user) {
+    return notFound();
+  }
+
+  const hasAccess = await prisma.project.findMany({
+    where: {
+      Tenant: {
+        slug: team,
+      },
+     id: Number(project),
+    },
+  });
+
+  if (hasAccess && hasAccess.length !== 1) {
+    return notFound();
+  }
+
+
   return (
     <>
       <DashboardShell>
