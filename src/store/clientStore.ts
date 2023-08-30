819197660e54d538e51054d1c758857260d4c419
kashifaxioned <kashifa@axioned.com>
2023-08-30 18:09:36 +0530

    clientlist store added

---
+++ src/store/clientStore.ts
@@ -0,0 +1,26 @@
+import { Status } from "@prisma/client";
+import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+
+// Define the interface of the Global state
+interface ClientState {
+  clients: {
+    id: number,
+    name: string,
+    status: Status
+  }[];
+  fetch: (team: string) => void;
+}
+
+export const useClientStore = create<ClientState>((set) => ({
+  clients: [],
+  fetch: async (team) => {
+    const response = await fetch(`/api/team/client/get?team=${team}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => res.json());
+    set({ clients: response });
+  },
+}));
