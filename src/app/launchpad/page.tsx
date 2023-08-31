933e85591a6872de64ec4f6474bb1baf26fbee29
kashifaxioned <kashifa@axioned.com>
2023-09-01 01:30:32 +0530

    Revert "Home component created"
    This reverts commit d8ac5534aa14ede94696844990d059fb705c519f.

--- src/app/launchpad/page.tsx
+++ src/app/launchpad/page.tsx
@@ -1,5 +1,7 @@
-import Home from "@/components/home";
+import { DashboardShell } from "@/components/ui/shell";
+import { DashboardHeader } from "@/components/ui/header";
 import { getCurrentUser } from "@/lib/session";
+import Link from "next/link";
 
 export const metadata = {
   title: "Launchpad",
@@ -8,6 +10,32 @@ export const metadata = {
 export default async function DashboardPage() {
   const user = await getCurrentUser();
   return (
-    <Home user={user}/>
+    <DashboardShell user={user}>
+      <DashboardHeader
+        heading="Your teams"
+        text="This is your launchpad 🚀. Select a team to get started."
+      ></DashboardHeader>
+      <div>
+        <div className="container flex flex-col items-center justify-center gap-12">
+          <h2>
+            Welcome back, <span>{user?.name}</span> 👋
+          </h2>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            {user?.tenants.map((team) => (
+              <Link
+                className="hover:bg-zinc/20 flex max-w-xs flex-col gap-4 rounded-xl bg-zinc-400/10 p-4  hover:bg-zinc-400/20"
+                href={"launchpad/" + team.slug}
+                key={team.id}
+              >
+                <h3>{team.name}</h3>
+                <div className="text-lg">
+                  The application lives here. Right now, its only layouts and static components.
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </DashboardShell>
   );
 }
--- src/components/ui/shell.tsx
+++ src/components/ui/shell.tsx
@@ -2,9 +2,20 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-interface DashboardShellProps extends React.HTMLAttributes<HTMLDivElement> {}
+import { useCurrentUserStore } from "@/store/currentuserstore";
+import { CurrenUserProps } from "@/types";
 
-export function DashboardShell({ children, className, ...props }: DashboardShellProps) {
+interface DashboardShellProps extends React.HTMLAttributes<HTMLDivElement> {
+  user?: CurrenUserProps
+}
+
+export function DashboardShell({ user, children, className, ...props }: DashboardShellProps) {
+  const [currentUser, fetchUser] = useCurrentUserStore(state => [state.currentUser, state.setCurrentUser])
+
+  React.useEffect(() => {
+    if(!currentUser) fetchUser(user) 
+  }, [currentUser, user])
+
   return (
     <div className={cn("grid items-start gap-8 p-2", className)} {...props}>
       {children}
