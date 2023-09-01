4f1760a709470049d195918b1b7c7d95702e3b3c
dshirlyaxioned <shirlyd@axioned.com>
2023-09-01 13:24:48 +0530

    feat: Log-426 routing wip

--- src/app/launchpad/[team]/projects/[project]/layout.tsx
+++ src/app/launchpad/[team]/projects/[project]/layout.tsx
@@ -1,21 +1,51 @@
 import { notFound } from "next/navigation";
-
+import { dashboardConfig } from "@/config/dashboard";
 import { getCurrentUser } from "@/lib/session";
+import { DashboardNav } from "@/components/nav";
+import { SidebarNavItem, projectProps } from "@/types";
 
-interface DashboardLayoutProps {
+interface DashboardLayoutProps extends projectProps {
   children?: React.ReactNode;
+ 
 }
 
-export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+export default async function DashboardLayout({ children, params }: DashboardLayoutProps) {
   const user = await getCurrentUser();
+  const projectId = params?.project;
+
+  const sidebarProjectsList: SidebarNavItem[] = [
+    {
+      title: "Milestones",
+      href: `/projects/${projectId}/milestones`,
+      icon: "post",
+    },
+    {
+      title: "Tasks",
+      href: `/projects/${projectId}/tasks`,
+      icon: "billing",
+    },
+    {
+      title: "Members",
+      href: `/projects/${projectId}/manage/members`,
+      icon: "user",
+    },
+    {
+      title: "Manage",
+      href: `/projects/${projectId}/manage`,
+      icon: "settings",
+    },
+  ]
 
   if (!user) {
     return notFound();
   }
 
   return (
-    <>
+    <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
+      <aside className="hidden w-[200px] flex-col md:flex">
+        <DashboardNav items={sidebarProjectsList} />
+      </aside>
       <div className="flex w-full flex-1 flex-col overflow-hidden">{children}</div>
-    </>
+    </div>
   );
 }
--- src/app/launchpad/[team]/projects/layout.tsx
+++ src/app/launchpad/[team]/projects/layout.tsx
@@ -1,8 +1,6 @@
 import { notFound } from "next/navigation";
 
-import { dashboardConfig } from "@/config/dashboard";
 import { getCurrentUser } from "@/lib/session";
-import { DashboardNav } from "@/components/nav";
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
@@ -17,10 +15,7 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
 
   return (
     <>
-      <div className="container grid gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav items={dashboardConfig.sidebarProjects} />
-        </aside>
+      <div>
         <main className="flex w-full flex-1 flex-col overflow-hidden">{children}</main>
       </div>
     </>
