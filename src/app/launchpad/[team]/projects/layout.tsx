24d8f458535df509407eb2c0d56357c1ff87c236
Shirly <86650374+dshirlyaxioned@users.noreply.github.com>
2023-09-01 14:06:39 +0530

    Project details page skeleton (#252)
    * Log-426 project detail page wip
    
    * feat: LOG-426 wip
    
    * feat: Log-426 routes based on project id wip
    
    * feat: Log-426 wip
    
    * feat: Log-426 routing wip
    
    * feat: Log-426 routing wip
    
    * feat: Log-426 project details skeleton done
    
    ---------
    
    Co-authored-by: Anurag Banerjee <anurag@axioned.com>

---
+++ src/app/launchpad/[team]/projects/layout.tsx
@@ -0,0 +1,23 @@
+import { notFound } from "next/navigation";
+
+import { getCurrentUser } from "@/lib/session";
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">{children}</main>
+      </div>
+    </>
+  );
+}
