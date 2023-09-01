97b83739290ee64bce071b75e68c4406ce3a5e01
kashifaxioned <kashifa@axioned.com>
2023-09-01 18:42:22 +0530

    client column added

--- src/app/launchpad/[team]/(team)/projects/columns.tsx
+++ src/app/launchpad/[team]/(team)/projects/columns.tsx
@@ -9,6 +9,7 @@ export type Projects = {
   id: number;
   name: string | null | undefined;
   status: Status;
+  clientName: string;
 };
 
 export const columns: ColumnDef<Projects>[] = [
@@ -23,4 +24,9 @@ export const columns: ColumnDef<Projects>[] = [
     header: ({ column }) => <DataTableColumnHeader column={column} title="Status" />,
     filterFn: "arrIncludesSome",
   },
+  {
+    accessorKey: "clientName",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Client" />,
+    filterFn: "arrIncludesSome",
+  }
 ];
