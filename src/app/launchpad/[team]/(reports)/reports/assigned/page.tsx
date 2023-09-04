23da8bb6e38cb60b23de83ae1ff2f0d402dc302c
kashifaxioned <kashifa@axioned.com>
2023-09-04 19:45:45 +0530

    get allocation using services

--- src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
+++ src/app/launchpad/[team]/(reports)/reports/assigned/page.tsx
@@ -1,20 +1,26 @@
 import { DashboardShell } from "@/components/ui/shell";
 import { DataTable } from "./data-table";
 import { DashboardHeader } from "@/components/ui/header";
-import { getProjectsId, getAllUsers } from "@/server/services/allocation";
+import { getProjectsId, getAllUsers, getAllocation } from "@/server/services/allocation";
 import { NewAllocationForm } from "@/components/forms/allocationForm";
 import type { Metadata } from "next";
 import { pageProps } from "@/types";
 import { getDynamicColumns } from "./columns";
+import dayjs from "dayjs";
+import { addDays } from "date-fns";
 
 export const metadata: Metadata = {
   title: `Assigned`,
 };
 
 export default async function Assigned({ params }: pageProps) {
+  const startDate = new Date()
+  const endDate = addDays(new Date(), 7)
   const { team } = params;
   const projects = await getProjectsId(team);
   const users = await getAllUsers(team);
+  // const allocationData = await getAllocation(team,)
+
   return (
     <>
       <DashboardShell>
