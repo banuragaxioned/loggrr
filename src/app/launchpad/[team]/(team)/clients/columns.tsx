18717984f6948b10d1bf72718b6afd47c7b659ac
vomkaraxioned <omkarv@axioned.com>
2023-09-05 19:23:27 +0530

    added new column and functionality

--- src/app/launchpad/[team]/(team)/clients/columns.tsx
+++ src/app/launchpad/[team]/(team)/clients/columns.tsx
@@ -3,17 +3,24 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/data-table-column-header";
 import { Status } from "@prisma/client";
+import Link from "next/link";
 
 export type Client = {
   id: number;
   name: string;
   status: Status;
+  Project:number;
 };
 
 export const columns: ColumnDef<Client>[] = [
   {
     accessorKey: "name",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Name" />,
+  },
+  {
+    accessorKey: "Project",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Projects No." />,
+    cell:({row})=><Link href={{pathname:"projects",search:`client=${row.original.name}`,}} title="Projects" className="tabular-nums">{row.original.Project}</Link>
   },
   {
     accessorKey: "status",
