4f97702fea2fd7a4b4808d61e361174b1abdb681
kashifaxioned <kashifa@axioned.com>
2023-09-06 12:42:53 +0530

    allocation type remove from services

--- src/server/services/allocation.ts
+++ src/server/services/allocation.ts
@@ -1,18 +1,5 @@
 import { prisma } from "../db";
 import { db } from "@/lib/db";
-import { AllocationFrequency } from "@prisma/client";
-import { AllocationDates } from "@/types";
-import dayjs from "dayjs";
-
-interface AllocationDate {
-  id: number;
-  billableTime: number;
-  nonBillableTime: number;
-  updatedAt: Date;
-  frequency: AllocationFrequency;
-  date: Date;
-  enddate: Date | any;
-}
 
 export async function getProjectsId(slug: string) {
   const projects = await db.project.findMany({
