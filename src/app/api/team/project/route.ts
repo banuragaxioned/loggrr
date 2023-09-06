1dc7ebcf4b9112f2d67a016f9d24d76bf1394cd8
dshirlyaxioned <shirlyd@axioned.com>
2023-09-06 19:21:06 +0530

    feat: add project api page

---
+++ src/app/api/team/project/route.ts
@@ -0,0 +1,48 @@
+import { getServerSession } from "next-auth/next";
+import * as z from "zod";
+import { authOptions } from "@/server/auth";
+import { db } from "@/lib/db";
+
+const addUserSchema = z.object({
+  team: z.string().min(1),
+  projectId: z.number(),
+  userId: z.number(),
+});
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const { user } = session;
+
+    const json = await req.json();
+    const body = addUserSchema.parse(json);
+
+    // check if the user has permission to the current team/tenant id if not return 403
+    // user session has an object (name, id, slug, etc) of all tenants the user has access to. i want to match slug.
+    if (user.tenants.filter((tenant) => tenant.slug === body.team).length === 0) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+
+    const project = await db.project.update({
+      where: { id: +body.projectId },
+      data: {
+        Members: {
+          connect: { id: body.userId },
+        },
+      },
+    });
+
+    return new Response(JSON.stringify(project));
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return new Response(null, { status: 500 });
+  }
+}
