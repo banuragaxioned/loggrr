11d4fb4559ea54ae379e22b94891e77c6c0ba4b3
Shirly <86650374+dshirlyaxioned@users.noreply.github.com>
2023-09-13 16:05:43 +0530

    Log-401 : Filter skills in reports assigned (#274)
    * feat: Log-401 filter skills in reports assigned
    
    * fix: PR changes on Log-401
    
    ---------
    
    Co-authored-by: Anurag Banerjee <anurag@axioned.com>

--- src/types/index.ts
+++ src/types/index.ts
@@ -178,4 +178,10 @@ export interface Assignment {
   userName: string;
   title: string;
   subRows: AllocationDetails[] | undefined;
+  skills?: Skills[];
+}
+
+interface Skills {
+  level: number,
+  skill: string,
 }
