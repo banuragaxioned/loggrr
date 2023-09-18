97efef60b1e0a28ee1a3357238aaa218284044da
Shirly <86650374+dshirlyaxioned@users.noreply.github.com>
2023-09-18 19:19:38 +0530

    Allow editing of Skill name (#282)
    * feat: Log-449 wip
    
    * feat: Log-449 edit skills table wip
    
    * feat: save icon added
    
    * fix: Log-449 pr changes
    
    * fix: toggle arrows in table
    
    * format
    
    ---------
    
    Co-authored-by: Anurag Banerjee <anurag@axioned.com>

--- src/app/launchpad/[team]/(skills)/skills/explore/columns.tsx
+++ src/app/launchpad/[team]/(skills)/skills/explore/columns.tsx
@@ -2,83 +2,71 @@
 
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
-import { Archive, ArrowUpDown } from "lucide-react";
-import { Summary } from "@/types";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { Input } from "@/components/ui/input";
+import { Dispatch } from "react";
+import { SkillUpdate } from "./data-table";
+import { DataTableColumnHeader } from "@/components/data-table-column-header";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-export const columns: ColumnDef<Summary>[] = [
-  {
-    accessorKey: "name",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="link"
-          className="text-slate-500"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Project
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+export interface SkillsList {
+  id: number;
+  name: string;
+  edit?: string;
+}
+
+export function skillName(editSkillNames: () => void, isEditing: SkillUpdate, setIsEditing: Dispatch<SkillUpdate>) {
+  const columns: ColumnDef<SkillsList>[] = [
+    {
+      accessorKey: "name",
+      header: ({ column }) => <DataTableColumnHeader column={column} title="Skills" />,
+      cell: ({ row }) => {
+        return isEditing?.id === row.original.id ? (
+          <Input
+            defaultValue={row.original.name}
+            onBlur={(e) => setIsEditing({ id: row.original.id, updatedValue: e.target.value })}
+            className="h-auto w-auto"
+          />
+        ) : (
+          row.original.name
+        );
+      },
     },
-  },
-  {
-    accessorKey: "budget",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="link"
-          className="text-slate-500"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Budget
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+    {
+      id: "edit",
+      cell: ({ row }: { row: any }) => {
+        return (
+          <div className={cn("invisible flex gap-x-3", "group-hover:visible")}>
+            {isEditing?.id === row.original.id ? (
+              <Button
+                title="Save"
+                className={cn("h-auto border-0 bg-inherit p-0")}
+                onClick={() => {
+                  editSkillNames();
+                  setIsEditing({ id: 0, updatedValue: "" });
+                }}
+              >
+                <Icons.save height={18} width={18} />
+              </Button>
+            ) : (
+              <Button
+                title="Edit"
+                className={cn("h-auto border-0 bg-inherit p-0")}
+                onClick={() => setIsEditing({ id: row.original.id, updatedValue: "" })}
+              >
+                <Icons.edit height={18} width={18} />
+              </Button>
+            )}
+          </div>
+        );
+      },
+      meta: {
+        className: "w-[10%]",
+      },
     },
-  },
-  {
-    accessorKey: "logged",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="link"
-          className="text-slate-500"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Logged
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-  },
-  {
-    accessorKey: "projectOwner",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="link"
-          className="text-slate-500"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Project Leads
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      return (
-        <div className="flex gap-3">
-          <Button className="border-0 bg-inherit p-0">
-            <Archive height={18} width={18} />
-          </Button>
-        </div>
-      );
-    },
-  },
-];
+  ];
+  return columns;
+}
