a4fba5639bbefcf712033f909e2e488850c69299
kashifaxioned <kashifa@axioned.com>
2023-09-26 00:12:08 +0530

    function added for find difference b/w to array

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,3 +32,7 @@ export function debounce(func: (...args: any[]) => void, delay: number) {
     }, delay);
   };
 }
+
+export function findDifferenceBetweenTwoArray(bigArray: any[], smallArray: any[]){
+  return bigArray.filter(obj1 => !smallArray.some(obj2 => obj1.id === obj2.id))
+}
