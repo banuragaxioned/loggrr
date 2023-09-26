a881894fb3c57edb71165e6ddae5e883886c3d8c
vomkaraxioned <omkarv@axioned.com>
2023-09-26 18:56:21 +0530

    wip

--- src/app/api/team/time-entry/route.tsx
+++ src/app/api/team/time-entry/route.tsx
@@ -56,8 +56,11 @@ export async function POST(req: Request) {
 
 
 export async function GET(req:Request) {
-  // const {userId,team} = new URL(req.url).searchParams;
-  console.log(new URL(req.url).searchParams)
+
+  const searchParams = new URL(req.url).searchParams;
+  const team = searchParams.get("team");
+  const date = searchParams.get("date");
+
   try {
     const session = await getServerSession(authOptions);
 
@@ -66,18 +69,34 @@ export async function GET(req:Request) {
     }
 
     const { user } = session;
-
-    // const json = await req.json();
-    // const body = json;
-
+    
     // check if the user has permission to the current team/tenant id if not return 403
     // user session has an object (name, id, slug, etc) of all tenants the user has access to. i want to match slug.
-    // if (user.tenants.filter((tenant) => tenant.slug === body.team).length === 0) {
-    //   return new Response("Unauthorized", { status: 403 });
-    // }
+    if (user.tenants.filter((tenant) => tenant.slug === team).length === 0) {
+      return new Response("Unauthorized", { status: 403 });
+    }
     //schema goes here
+    const response = await db.timeEntry.findMany({
+      where:{
+        userId:user.id,
+        // date:date ? date :"",
+        Tenant:{
+          slug:team ? team : ""
+        }
+      },
+      select:{
+       id:true,
+       billable:true,
+       comments:true,
+       milestoneId:true,
+       projectId:true,
+       taskId:true,
+       time:true,
+       date:true
+      }
+    });
 
-    return new Response(JSON.stringify([]));
+    return new Response(JSON.stringify(response));
   } catch (error) {
     if (error instanceof z.ZodError) {
       return new Response(JSON.stringify(error.issues), { status: 422 });
--- src/components/time-entries-list.tsx
+++ src/components/time-entries-list.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 interface TimeEntriesListProps {
     userId:number;
@@ -7,24 +8,28 @@ interface TimeEntriesListProps {
 }
 
 export const TimeEntriesList = ({userId,team}:TimeEntriesListProps)=> {
+    //0 = loading, 1 = loaded with success , -1 = failed to fetch
+    const [entries,setEntries] = useState({data:[],status:0});
+    const getEntries =  fetch( `/api/team/time-entry?team=${team}&date=${new Date()}`,{
+            method:"GET",
+            headers: {
+                "Content-Type": "application/json",
+              }
+        });
+        
 
-    const [entries,setEntries] = useState([]);
-    const getEntries =  fetch( `/api/team/time-entry?userId=${userId},team=${team}`,{
-        method:"GET",
-        headers: {
-            "Content-Type": "application/json",
-          }
-    });
-
-    getEntries.then(res=>console.log(res))
-
-
+    getEntries.then(res=>res.json()).then(res=>setEntries({data:res,status:1})).catch((e)=>setEntries({data:[],status:-1}))
+        console.log(entries)
     return (
-        <ul>
+        <ul className="w-3/5 mx-auto">
             {
-                entries.map(entry=>(<li>
-                    {entry}
-                </li>))
+               entries?.status === 0 ? 
+               <Skeleton className="w-full h-20"/>:
+               entries?.status > 0 ?
+               <></>:
+               <li>
+                sorry something went wrong
+               </li>
             }
         </ul>
     )
