07a8b43d670285e31f31666c3b190f0048bde58a
dshirlyaxioned <shirlyd@axioned.com>
2023-09-27 16:46:51 +0530

    fix: Log-462 and Log-463

--- src/components/data-table-column-header.tsx
+++ src/components/data-table-column-header.tsx
@@ -33,7 +33,7 @@ export function DataTableColumnHeader<TData, TValue>({
       <Button
         variant="ghost"
         size="sm"
-        className="h-8 px-0 data-[state=open]:bg-accent"
+        className="h-8 p-3 data-[state=open]:bg-accent"
         onClick={() => clickHandler(column)}
       >
         {title && <span>{title}</span>}
