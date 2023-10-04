3c972dad59cbd3560e056f4a7739521766a42469
Shirly <86650374+dshirlyaxioned@users.noreply.github.com>
2023-10-04 19:09:05 +0530

    Log-464 : Assignment route - make Week dropdown to be a switch/toggle (#298)
    * feat: Log-464 done
    
    * fix: build issue
    
    * fix: button style

--- src/components/data-table-faceted-filter.tsx
+++ src/components/data-table-faceted-filter.tsx
@@ -38,7 +38,7 @@ export function DataTableFacetedFilter<TData, TValue>({
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant="outline" size="sm" className="h-8 border-dashed">
+        <Button variant="outline" size="sm" className="h-10 border-dashed">
           <Icons.add className="mr-2 h-4 w-4" />
           {title}
           {selectedValues?.size > 0 && (
