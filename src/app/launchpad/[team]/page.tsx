13bda0f4ac9255b13cb4891c8bb1152dde26e2f1
dshirlyaxioned <shirlyd@axioned.com>
2023-10-10 18:26:57 +0530

    feat: Log-453 added tremor package for homescreen time widget

--- src/app/launchpad/[team]/page.tsx
+++ src/app/launchpad/[team]/page.tsx
@@ -2,9 +2,37 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { getCurrentUser } from "@/lib/session";
 
 import { notFound } from "next/navigation";
+import { Card, Text, Flex, CategoryBar } from "@tremor/react";
+import { db } from "@/lib/db";
 
 export default async function Dashboard() {
   const user = await getCurrentUser();
+
+  const userTimeAllocated = await db.allocation.findMany({
+    where: {
+      userId: user?.id,
+    },
+    select: {
+      billableTime: true
+    }
+  })
+
+  const userTimeEntry = await db.timeEntry.findMany({
+    where: {
+      userId: user?.id
+    },
+    select: {
+      time: true
+    }
+  })
+
+  // Add all the billable time if user present in more than 1 project
+  function sumArray(sum: number, num: number): number {
+    return sum + num;
+  }
+
+  const overallAllocatedTime = userTimeAllocated.map((item) => item.billableTime).reduce(sumArray);
+  const overallEntryTime = userTimeEntry.map((item) => item.time).reduce(sumArray);
 
   if (!user) {
     return notFound();
@@ -23,9 +51,18 @@ export default async function Dashboard() {
       <aside className="col-span-3 m-2 hidden space-y-12 lg:block lg:basis-1/4">
         {/* Quick stats (% of time logged in the last week) */}
         <div className="flex flex-col items-center gap-4">
-          <Skeleton className="h-8 w-60" />
-          <Skeleton className="h-4 w-60" />
-          <Skeleton className="h-4 w-60" />
+          <Card className="w-60 mx-auto p-4 pb-6">
+            <Text className="pb-5 text-base font-semibold">Logged hours</Text>
+            <Flex>
+              <Text className="text-sm pb-4"><span className="text-3xl font-semibold">{overallEntryTime}</span> / {overallAllocatedTime}h</Text>
+            </Flex>
+            <CategoryBar
+              values={[25, 25, 25, 25]}
+              colors={["rose", "orange", "yellow", "emerald"]}
+              markerValue={overallEntryTime / overallAllocatedTime * 100}
+              className="mt-3 text-sm"
+            />
+          </Card>
         </div>
         {/* Time Insights (breakdown of time over the last week) */}
         <div className="flex flex-col items-center gap-4">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
     ".src/components/**/*.{js,jsx,ts,tsx}",
     ".src/app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
   ],
   theme: {
     container: {
@@ -61,6 +62,54 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        tremor: {
+          // light mode
+          brand: {
+            DEFAULT: "#3b82f6", // blue-500
+            inverted: "#ffffff", // white
+          },
+          background: {
+            DEFAULT: "#ffffff", // white
+          },
+          border: {
+            DEFAULT: "#CBD5E1", // gray-200
+          },
+          ring: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          content: {
+            DEFAULT: "#374151", // gray-500
+          },
+        },
+        "dark-tremor": {
+          // dark mode
+          brand: {
+            DEFAULT: "#3b82f6", // blue-500
+            inverted: "#ffffff", // white
+          },
+          background: {
+            DEFAULT: "#18181B", // gray-900
+          },
+          border: {
+            DEFAULT: "#3F3F46", // gray-800
+          },
+          ring: {
+            DEFAULT: "#1f2937", // gray-800
+          },
+          content: {
+            DEFAULT: "#ffffff", // gray-500
+          },
+        },
+      },
+      boxShadow: {
+        // light
+        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        // dark
+        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "dark-tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "dark-tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
       },
       fontFamily: {
         sans: ["var(--font-primary)", ...fontFamily.sans],
@@ -70,6 +119,9 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
       },
       keyframes: {
         "accordion-down": {
@@ -87,6 +139,35 @@ module.exports = {
       },
     },
   },
+  safelist: [
+    {
+      pattern:
+        /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+  ],
   // @ts-ignore
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
 };
