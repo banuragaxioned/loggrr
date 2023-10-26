95ba9e29a3e3ee924da9979b50b1415055b411d3
Shirly <86650374+dshirlyaxioned@users.noreply.github.com>
2023-10-26 19:30:10 +0530

    Log 468 : Custom Marker bar (#305)
    * wip:Project changes
    
    * wip:ui added
    
    * api added
    
    * time entry form added
    
    * removed any type
    
    * removed console
    
    * PR Changes
    
    * wip
    
    * wip
    
    * wip
    
    * time entry list added
    
    * time fix added
    
    * inline calendar added
    
    * wip
    
    * timelog component optimized
    
    * wip
    
    * added local storage
    
    * wip:time log form integration
    
    * wip:refactoring time log component
    
    * feat: Log-453 added tremor package for homescreen time widget
    
    * fix: total logged hours
    
    * wip :time synchronization added
    
    * feat: Log-454 wip
    
    * time log form 3 components synchronized
    
    * border added
    
    * Filled time indicator flag added
    
    * tremor chart implementation
    
    * time log form refetching issue fix
    
    * removed console log
    
    * trim method added to comment string
    
    * form submission issue fix
    
    * time calculation changes
    
    * feat: log-454 wip
    
    * edit functionality added
    
    * log-454 wip
    
    * log-454 done
    
    * removed unused component
    
    * feat: custom marker bar done
    
    * removed unnecessary comments
    
    * Added comment on marker bar component
    
    * feat: added summary progress in home side nav
    
    ---------
    
    Co-authored-by: vomkaraxioned <omkarv@axioned.com>
    Co-authored-by: Vipin Yadav <vipiny35@gmail.com>

--- src/app/launchpad/[team]/page.tsx
+++ src/app/launchpad/[team]/page.tsx
@@ -4,8 +4,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { TimeEntry } from "@/components/time-entry";
 import { getCurrentUser } from "@/lib/session";
 import { pageProps } from "@/types";
-import { Card, Text, Flex, CategoryBar } from "@tremor/react";
 import { db } from "@/lib/db";
+import {
+  Card,
+  Text,
+  Flex,
+  CategoryBar,
+  TabList,
+  Tab,
+  TabGroup,
+  TabPanels,
+  TabPanel,
+  ProgressBar,
+} from "@tremor/react";
+import { Icons } from "@/components/icons";
+import { MarkerBar } from "@/components/marker-bar";
 
 export default async function Dashboard({ params }: pageProps) {
   const user = await getCurrentUser();
@@ -15,15 +28,46 @@ export default async function Dashboard({ params }: pageProps) {
     return notFound();
   }
 
+  const userTimeAllocated = await db.allocation.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      billableTime: true,
+      nonBillableTime: true,
+      Project: {
+        select: {
+          id: true,
+          name: true,
+        }
+      },
+      User: {
+        select: {
+          TimeEntry: {
+            select: {
+              projectId: true,
+              time: true,
+            }
+          }
+        }
+      }
+    }
+  })
   const projects = await getAllUserProjects(user.id);
+
   const userTimeEntry = await db.timeEntry.findMany({
     where: {
       userId: user.id,
     },
     select: {
       time: true,
-    },
-  });
+      Project: {
+        select: {
+          name: true,
+        }
+      },
+    }
+  })
 
   const overallEntryTime = userTimeEntry.map((item) => item.time).reduce((sum: number, num: number) => sum + num, 0);
 
@@ -36,8 +80,14 @@ export default async function Dashboard({ params }: pageProps) {
       <aside className="col-span-12 m-2 hidden space-y-12 md:col-span-3 lg:block lg:basis-1/4">
         {/* Quick stats (% of time logged in the last week) */}
         <div className="flex flex-col items-center gap-4">
-          <Card className="mx-auto w-60 p-4 pb-6">
-            <Text className="pb-5 text-base font-semibold">Logged hours</Text>
+          <Card className="mx-auto w-full p-4 pb-6">
+            <Flex className="font-semibold items-center">
+              <Text className="pb-5 text-base bg-gradient-to-r from-green-to-red">Logged hours</Text>
+              <Text className="text-[#6B7280] text-xs flex items-center pb-5">
+                <Icons.calendar className="ml-2 h-4 w-4 mr-[5px]" />
+                This week
+              </Text>
+            </Flex>
             <Flex>
               <Text className="pb-4 text-sm">
                 <span className="text-3xl font-semibold">{overallEntryTime}</span> / 40h
@@ -49,6 +99,41 @@ export default async function Dashboard({ params }: pageProps) {
               markerValue={(overallEntryTime / 40) * 100}
               className="mt-3 text-sm"
             />
+            {/* Time Insights (breakdown of time based on projects) */}
+            <Text className="pb-5 pt-8 text-base font-semibold">Time logged</Text>
+            <TabGroup>
+              <TabList className="">
+                <Tab>Projects</Tab>
+                <Tab>Assigned</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  {userTimeEntry.length === 0 ? <p>Log your hours</p> : userTimeEntry.map((item, i) => (
+                    <div className="mt-8" key={i}>
+                      <Text className="w-full font-semibold text-black leading-5">{item.Project.name}</Text>
+                      <Flex className="items-center mt-3">
+                        <ProgressBar value={item.time} color="indigo" className="mr-4" />
+                        <Text className="text-gray-500 text-sm font-normal">{Math.round(item.time / 40 * 100)}%</Text>
+                      </Flex>
+                    </div>
+                  ))}
+                </TabPanel>
+                <TabPanel>
+                  {userTimeAllocated.map((item, i) => {
+                    const entryValue = item.User.TimeEntry.filter((ele) => ele.projectId === item.Project.id).reduce((acc, current) => acc + current.time, 0);
+                    return (
+                      <div className="mt-8" key={i}>
+                        <Text className="w-full font-semibold text-black leading-5">{item.Project.name}</Text>
+                        <Flex className="items-center mt-3">
+                          <MarkerBar value={item.billableTime + item.nonBillableTime} minValue={0} maxValue={entryValue} color="slate" className="mr-4 relative w-full bg-slate-200 rounded-md" />
+                          <Text className="text-gray-500 text-sm font-normal">{entryValue}h</Text>
+                        </Flex>
+                      </div>
+                    )
+                  })}
+                </TabPanel>
+              </TabPanels>
+            </TabGroup>
           </Card>
         </div>
         {/* Time Insights (breakdown of time over the last week) */}
