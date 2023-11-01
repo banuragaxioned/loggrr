97a519a61f171e3a2abb803e174516538c8bba94
dshirlyaxioned <shirlyd@axioned.com>
2023-11-01 20:09:50 +0530

    fix: side bar wip

--- src/app/launchpad/[team]/page.tsx
+++ src/app/launchpad/[team]/page.tsx
@@ -72,12 +72,15 @@ export default async function Dashboard({ params }: pageProps) {
 
   const overallEntryTime = userTimeEntry.map((item) => item.time).reduce((sum: number, num: number) => sum + num, 0);
 
-  // const toolTip = () => {
-  //   const value = `${(overallEntryTime / 40) * 100}%`
-  //   return (
-  //     <p className="p-3 border border-blue-200">{value}</p>
-  //   )
-  // }
+  const toolTip = () => {
+    const value = `${(overallEntryTime / 40) * 100}%`
+    return (
+      <p className="p-3 border border-blue-200">{value}</p>
+    )
+  }
+
+  // console.log(toolTip());
+  
   
   return (
     <div className="col-span-12 grid w-full grid-cols-12">
@@ -88,7 +91,7 @@ export default async function Dashboard({ params }: pageProps) {
       <aside className="col-span-12 hidden space-y-12 md:col-span-3 lg:block lg:basis-1/4">
         {/* Quick stats (% of time logged in the last week) */}
         <div className="flex flex-col items-center gap-4">
-          <Card className="mx-auto w-full p-4 pb-6">
+          <Card className="mx-auto w-[95%] p-4 pb-6 side-bar">
             <Flex className="font-semibold items-center">
               <Text className="pb-5 text-base bg-gradient-to-r from-green-to-red">Logged hours</Text>
               <Text className="text-[#6B7280] text-xs flex items-center pb-5">
@@ -105,8 +108,8 @@ export default async function Dashboard({ params }: pageProps) {
               values={[25, 25, 25, 25]}
               colors={["rose", "orange", "yellow", "emerald"]}
               markerValue={(overallEntryTime / 40) * 100}
-              className="mt-3 text-sm"
-              // tooltip={toolTip}
+              className="mt-3 text-sm "
+              tooltip={`${Math.round((overallEntryTime / 40) * 100)}%`}
             />
             {/* Time Insights (breakdown of time based on projects) */}
             <Text className="pb-5 pt-8 text-base font-semibold">Time logged</Text>
@@ -116,8 +119,8 @@ export default async function Dashboard({ params }: pageProps) {
                 <Tab>Assigned</Tab>
               </TabList>
               <TabPanels>
-                <TabPanel>
-                  <ScrollArea className="max-h-[256px] w-full">
+                <TabPanel className="max-h-[500px] mt-0">
+                  <ScrollArea className={` ${userTimeEntry.length > 5 ? "h-[500px]" : "h-auto"} w-full}`}>
                     {userTimeEntry.map((item, i) => (
                       <div className="mt-8 pr-3" key={i}>
                         <Text className="w-full font-semibold text-black leading-5">{item.Project.name}</Text>
@@ -147,24 +150,12 @@ export default async function Dashboard({ params }: pageProps) {
             </TabGroup>
           </Card>
         </div>
-        {/* Time Insights (breakdown of time over the last week) */}
+        {/* Time Insights (breakdown of time over the last week)
         <div className="flex flex-col items-center gap-4">
           <Skeleton className="h-8 w-60" />
           <Skeleton className="h-4 w-60" />
           <Skeleton className="h-4 w-60" />
-        </div>
-        {/* Advanced Analytics (comparision vs. peer average, and vs. self from the previous week) */}
-        <div className="flex flex-col items-center gap-4">
-          <Skeleton className="h-8 w-60" />
-          <Skeleton className="h-4 w-60" />
-          <Skeleton className="h-4 w-60" />
-        </div>
-        {/* Quick polls and survey */}
-        <div className="flex flex-col items-center gap-4">
-          <Skeleton className="h-8 w-60" />
-          <Skeleton className="h-4 w-60" />
-          <Skeleton className="h-4 w-60" />
-        </div>
+        </div> */}
       </aside>
     </div>
   );
--- src/components/marker-bar.tsx
+++ src/components/marker-bar.tsx
@@ -19,9 +19,10 @@ export function MarkerBar({ value, minValue, maxValue, className }: MarkerBarPro
   // Render the marker bar
   return (
     <div className={className}>
-      <div className='bg-slate-200 rounded-md w-full h-2 overflow-hidden' />
-      <div className='bg-[#f43f5e] absolute top-0 left-0 h-full rounded-md' style={{ width: `${maxValue}%` }} />
-      <div className='bg-[#32ba80] absolute top-0 left-0 h-full rounded-md' style={{ width: `${getGreenBarWidth()}%` }} />
+      <div className='bg-slate-200 rounded-md w-full h-2 overflow-hidden'>
+        <div className='bg-[#f43f5e] absolute top-0 left-0 h-full rounded-md max-w-full' style={{ width: `${maxValue}%` }} />
+        <div className='bg-[#32ba80] absolute top-0 left-0 h-full rounded-md max-w-full' style={{ width: `${getGreenBarWidth()}%` }} />
+      </div>
       <div className={`h-[20px] w-[6px] rounded-md bg-slate-400 border-white border-[1px] absolute -top-[5px]`} style={{ left: `${value}%` }} />
     </div>
   );
