cdc98a77136782681e5f0ee34774e43b73afc853
Anurag Banerjee <anurag@axioned.com>
2023-11-09 12:52:35 +0530

    Package updates and minor cleanup (#329)
    * Update most packages
    
    * Remove now unsupported meta data
    
    * Update remaining packages (react-email)
    
    * Remove now unsupported pX and pY
    
    * Remove some unused imports

--- src/app/launchpad/[team]/page.tsx
+++ src/app/launchpad/[team]/page.tsx
@@ -1,24 +1,13 @@
 import { notFound } from "next/navigation";
-import { getProjectSummary, getAllUserProjects } from "@/server/services/project";
-import { Skeleton } from "@/components/ui/skeleton";
+import { getAllUserProjects } from "@/server/services/project";
 import { TimeEntry } from "@/components/time-entry";
 import { getCurrentUser } from "@/lib/session";
 import { pageProps } from "@/types";
 import { db } from "@/lib/db";
-import {
-  Text,
-  Flex,
-  CategoryBar,
-  TabList,
-  Tab,
-  TabGroup,
-  TabPanels,
-  TabPanel,
-  ProgressBar,
-} from "@tremor/react";
+import { Text, Flex, CategoryBar, TabList, Tab, TabGroup, TabPanels, TabPanel, ProgressBar } from "@tremor/react";
 import { Icons } from "@/components/icons";
 import { MarkerBar } from "@/components/marker-bar";
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default async function Dashboard({ params }: pageProps) {
   const user = await getCurrentUser();
@@ -39,7 +28,7 @@ export default async function Dashboard({ params }: pageProps) {
         select: {
           id: true,
           name: true,
-        }
+        },
       },
       User: {
         select: {
@@ -47,12 +36,12 @@ export default async function Dashboard({ params }: pageProps) {
             select: {
               projectId: true,
               time: true,
-            }
-          }
-        }
-      }
-    }
-  })
+            },
+          },
+        },
+      },
+    },
+  });
   const projects = await getAllUserProjects(user.id);
 
   const userTimeEntry = await db.timeEntry.findMany({
@@ -64,10 +53,10 @@ export default async function Dashboard({ params }: pageProps) {
       Project: {
         select: {
           name: true,
-        }
+        },
       },
-    }
-  })
+    },
+  });
 
   const overallEntryTime = userTimeEntry.map((item) => item.time).reduce((sum: number, num: number) => sum + num, 0);
 
@@ -80,11 +69,11 @@ export default async function Dashboard({ params }: pageProps) {
       <aside className="col-span-12 hidden space-y-12 md:col-span-3 lg:block lg:basis-1/4">
         {/* Quick stats (% of time logged in the last week) */}
         <div className="flex flex-col items-center gap-4">
-          <div className="mx-auto max-w-xs w-full border border-border rounded-xl p-4 pb-6 side-bar">
-            <Flex className="font-semibold items-center">
-              <Text className="pb-5 text-base bg-gradient-to-r from-green-to-red">Logged hours</Text>
-              <Text className="text-[#6B7280] text-xs flex items-center pb-5">
-                <Icons.calendar className="ml-2 h-4 w-4 mr-[5px]" />
+          <div className="side-bar mx-auto w-full max-w-xs rounded-xl border border-border p-4 pb-6">
+            <Flex className="items-center font-semibold">
+              <Text className="from-green-to-red bg-gradient-to-r pb-5 text-base">Logged hours</Text>
+              <Text className="flex items-center pb-5 text-xs text-[#6B7280]">
+                <Icons.calendar className="ml-2 mr-[5px] h-4 w-4" />
                 Current week
               </Text>
             </Flex>
@@ -108,40 +97,53 @@ export default async function Dashboard({ params }: pageProps) {
                 <Tab>Assigned</Tab>
               </TabList>
               <TabPanels>
-                <TabPanel className="max-h-[500px] mt-0">
+                <TabPanel className="mt-0 max-h-[500px]">
                   <ScrollArea className={`${userTimeEntry.length >= 5 ? "h-[500px]" : "h-auto"} w-full}`}>
-                    {userTimeEntry.length === 0 ?
+                    {userTimeEntry.length === 0 ? (
                       <div className="mt-8 pr-3">
-                        <Text className="w-full font-semibold text-black leading-5">Projects</Text>
-                        <Flex className="items-center mt-3">
+                        <Text className="w-full font-semibold leading-5 text-black">Projects</Text>
+                        <Flex className="mt-3 items-center">
                           <ProgressBar value={0} color="indigo" className="mr-4" />
-                          <Text className="text-gray-500 text-sm font-normal">0%</Text>
+                          <Text className="text-sm font-normal text-gray-500">0%</Text>
                         </Flex>
-                      </div> :
+                      </div>
+                    ) : (
                       userTimeEntry.map((item, i) => (
                         <div className="mt-8 pr-3" key={i}>
-                          <Text className="w-full font-semibold text-black leading-5">{item.Project.name}</Text>
-                          <Flex className="items-center mt-3">
+                          <Text className="w-full font-semibold leading-5 text-black">{item.Project.name}</Text>
+                          <Flex className="mt-3 items-center">
                             <ProgressBar value={item.time} color="indigo" className="mr-4" />
-                            <Text className="text-gray-500 text-sm font-normal">{Math.round(item.time / 40 * 100)}%</Text>
+                            <Text className="text-sm font-normal text-gray-500">
+                              {Math.round((item.time / 40) * 100)}%
+                            </Text>
                           </Flex>
                         </div>
-                      ))}
+                      ))
+                    )}
                   </ScrollArea>
                 </TabPanel>
-                <TabPanel className="max-h-[550px] mt-0">
+                <TabPanel className="mt-0 max-h-[550px]">
                   <ScrollArea className={`${userTimeAllocated.length >= 5 ? "h-[550px]" : "h-auto"} w-full}`}>
                     {userTimeAllocated.map((item, i) => {
-                      const entryValue = item.User.TimeEntry.filter((ele) => ele.projectId === item.Project.id).reduce((acc, current) => acc + current.time, 0);
+                      const entryValue = item.User.TimeEntry.filter((ele) => ele.projectId === item.Project.id).reduce(
+                        (acc, current) => acc + current.time,
+                        0,
+                      );
                       return (
                         <div className="mt-8 pr-3" key={i}>
-                          <Text className="w-full font-semibold text-black leading-5">{item.Project.name}</Text>
-                          <Flex className="items-center mt-3">
-                            <MarkerBar value={item.billableTime + item.nonBillableTime} minValue={0} maxValue={entryValue} color="slate" className="mr-4 relative w-full bg-slate-200 rounded-md" />
-                            <Text className="text-gray-500 text-sm font-normal">{entryValue}h</Text>
+                          <Text className="w-full font-semibold leading-5 text-black">{item.Project.name}</Text>
+                          <Flex className="mt-3 items-center">
+                            <MarkerBar
+                              value={item.billableTime + item.nonBillableTime}
+                              minValue={0}
+                              maxValue={entryValue}
+                              color="slate"
+                              className="relative mr-4 w-full rounded-md bg-slate-200"
+                            />
+                            <Text className="text-sm font-normal text-gray-500">{entryValue}h</Text>
                           </Flex>
                         </div>
-                      )
+                      );
                     })}
                   </ScrollArea>
                 </TabPanel>
