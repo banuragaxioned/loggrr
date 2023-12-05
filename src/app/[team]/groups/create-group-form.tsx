13bb68e46c477d4950c74f3bc8ba002f28c42a65
Anurag Banerjee <anurag@axioned.com>
2023-12-06 00:56:09 +0530

    Remove unused import

--- src/app/[team]/groups/create-group-form.tsx
+++ src/app/[team]/groups/create-group-form.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import useToast from "@/hooks/useToast";
 import { SheetWrapper } from "@/components/ui/sheet-wrapper";
