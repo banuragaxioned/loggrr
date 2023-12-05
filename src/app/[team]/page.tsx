5234a4a9c1eeec1fa6df56779455256e3bb558d5
Anurag Banerjee <anurag@axioned.com>
2023-12-05 20:53:03 +0530

    Refactor time assigned card in Dashboard

--- src/app/[team]/page.tsx
+++ src/app/[team]/page.tsx
@@ -4,8 +4,7 @@ import { TimeEntry } from "@/components/time-entry";
 import { getCurrentUser } from "@/lib/session";
 import { pageProps } from "@/types";
 import { db } from "@/lib/db";
-import { Text, Flex, CategoryBar, TabList, Tab, TabGroup, TabPanels, TabPanel, ProgressBar } from "@tremor/react";
-import { CalendarDays, Camera } from "lucide-react";
+import { Text, Flex, Card } from "@tremor/react";
 import { MarkerBar } from "@/components/marker-bar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import CategoryDataBar from "@/components/charts/category-bar";
@@ -75,73 +74,35 @@ export default async function Dashboard({ params }: pageProps) {
           maxValue={37.5}
           type="hours"
         />
-        <div className="flex flex-col items-center gap-4">
-          <div className="side-bar mx-auto w-full max-w-xs rounded-xl border border-border p-4 pb-6">
-            {/* Time Insights (breakdown of time based on projects) */}
-            <Text className="pb-5 pt-8 text-base font-semibold">Time logged</Text>
-            <TabGroup>
-              <TabList>
-                <Tab>Projects</Tab>
-                <Tab>Assigned</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel className="mt-0 max-h-[500px]">
-                  <ScrollArea className={`${userTimeEntry.length >= 5 ? "h-[500px]" : "h-auto"} w-full}`}>
-                    {userTimeEntry.length === 0 ? (
-                      <div className="mt-8 pr-3">
-                        <Text className="w-full font-semibold leading-5">Projects</Text>
-                        <Flex className="mt-3 items-center">
-                          <ProgressBar value={0} color="indigo" className="mr-4" />
-                          <Text className="text-sm font-normal">0%</Text>
-                        </Flex>
-                      </div>
-                    ) : (
-                      userTimeEntry.map((item, i) => (
-                        <div className="mt-8 pr-3" key={i}>
-                          <Text className="w-full font-semibold leading-5">{item.Project.name}</Text>
-                          <Flex className="mt-3 items-center">
-                            <ProgressBar
-                              value={item.time / 60}
-                              color="indigo"
-                              className="mr-4"
-                              tooltip={`${Math.round((overallEntryTime / 60 / 37.5) * 100)}%`}
-                            />
-                            <Text className="font-norma text-sm">{Math.round((item.time / 60 / 37.5) * 100)}%</Text>
-                          </Flex>
-                        </div>
-                      ))
-                    )}
-                  </ScrollArea>
-                </TabPanel>
-                <TabPanel className="mt-0 max-h-[550px]">
-                  <ScrollArea className={`${userTimeAllocated.length >= 5 ? "h-[550px]" : "h-auto"} w-full}`}>
-                    {userTimeAllocated.map((item, i) => {
-                      const entryValue = item.User.TimeEntry.filter((ele) => ele.projectId === item.Project.id).reduce(
-                        (acc, current) => acc + current.time,
-                        0,
-                      );
-                      return (
-                        <div className="mt-8 pr-3" key={i}>
-                          <Text className="w-full font-semibold leading-5">{item.Project.name}</Text>
-                          <Flex className="mt-3 items-center">
-                            <MarkerBar
-                              value={item.billableTime + item.nonBillableTime}
-                              minValue={0}
-                              maxValue={entryValue}
-                              color="slate"
-                              className="relative mr-4 w-full rounded-md "
-                            />
-                            <Text className="text-sm font-normal">{entryValue}h</Text>
-                          </Flex>
-                        </div>
-                      );
-                    })}
-                  </ScrollArea>
-                </TabPanel>
-              </TabPanels>
-            </TabGroup>
-          </div>
-        </div>
+        {/* TODO: Need to refactor and make the below card reusable like CategoryDataBar */}
+        <Card className="border border-border">
+          <Flex className="items-center font-semibold">
+            <Text className="pb-2">Time assigned</Text>
+          </Flex>
+          <ScrollArea className={`${userTimeAllocated.length >= 5 ? "h-[550px]" : "h-auto"} w-full}`}>
+            {userTimeAllocated.map((item, i) => {
+              const entryValue = item.User.TimeEntry.filter((ele) => ele.projectId === item.Project.id).reduce(
+                (acc, current) => acc + current.time,
+                0,
+              );
+              return (
+                <div className="mt-3 pr-3" key={i}>
+                  <Text className="w-full font-semibold leading-5">{item.Project.name}</Text>
+                  <Flex className="mt-3 items-center">
+                    <MarkerBar
+                      value={item.billableTime + item.nonBillableTime}
+                      minValue={0}
+                      maxValue={entryValue}
+                      color="slate"
+                      className="relative mr-4 w-full rounded-md "
+                    />
+                    <Text className="text-sm font-normal">{entryValue}h</Text>
+                  </Flex>
+                </div>
+              );
+            })}
+          </ScrollArea>
+        </Card>
       </aside>
     </div>
   );
