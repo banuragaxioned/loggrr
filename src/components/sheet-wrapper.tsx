a3fe34e31b9386f0d02eb01f5fe6a928b06965d8
Anurag Banerjee <anurag@axioned.com>
2023-12-05 23:53:17 +0530

    Add alert and sheet wrapper

---
+++ src/components/sheet-wrapper.tsx
@@ -0,0 +1,26 @@
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode;
+  button: string;
+  title: string;
+  description: string;
+}
+
+export function SheetWrapper({ children, button, title, description }: DashboardLayoutProps) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">{button}</Button>
+      </SheetTrigger>
+      <SheetContent position="right" size="sm">
+        <SheetHeader>
+          <SheetTitle>{title}</SheetTitle>
+          <SheetDescription>{description}</SheetDescription>
+        </SheetHeader>
+        {children}
+      </SheetContent>
+    </Sheet>
+  );
+}
