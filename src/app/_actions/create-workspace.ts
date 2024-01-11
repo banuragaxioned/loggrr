def31236284225ed2aa200f5680d3dfc113b97d7
banuragaxioned <anurag@axioned.com>
2024-01-12 00:09:20 +0530

    Add new server actions

---
+++ src/app/_actions/create-workspace.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { db } from "@/db";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(3),
+  slug: z.string().min(3),
+});
+
+type Form = z.infer<typeof schema>;
+
+export async function createWorkspace(formData: Form) {
+  try {
+    await db.workspace.create({
+      data: {
+        name: formData.name,
+        slug: formData.slug,
+      },
+    });
+  } catch (e) {
+    throw new Error("Failed to create workspace");
+  }
+  return { success: true };
+}
