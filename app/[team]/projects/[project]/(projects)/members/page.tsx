d286e5d1c2297e510ab2fc4451efc7a29ae3f244
Vipin Yadav <vipiny35@gmail.com>
2024-01-24 13:49:42 +0530

    Add project members page and db function

--- app/[team]/projects/[project]/(projects)/members/page.tsx
+++ app/[team]/projects/[project]/(projects)/members/page.tsx
@@ -2,13 +2,20 @@ import { Button } from "@/components/ui/button";
 import { DashboardHeader } from "@/components/ui/header";
 import { DashboardShell } from "@/components/ui/shell";
 import { pageProps } from "@/types";
+import { Table } from "./table";
+import { columns } from "./columns";
+import { getProjectMembers } from "../../../../../../server/services/members";
 
 export default async function Page({ params }: pageProps) {
+  const { team, project } = params;
+  const members = await getProjectMembers({ projectId: +project!, team: team });
+
   return (
     <DashboardShell>
-      <DashboardHeader heading="Manage Project team" text="People who are assigned to this project">
+      <DashboardHeader heading="Manage project team" text="People who are assigned to this project">
         <Button variant="outline">Edit</Button>
       </DashboardHeader>
+      <Table columns={columns} data={members} />
     </DashboardShell>
   );
 }
