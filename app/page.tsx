3c7d27200e212eb1f31b909d7f7e5b950518405d
banuragaxioned <anurag@axioned.com>
2024-01-31 23:23:02 +0530

    Add the new home route sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,36 @@
-import { buttonVariants } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { getCurrentUser } from "@/server/session";
-import { PartyPopper } from "lucide-react";
-import Link from "next/link";
+import { HeroSection } from "./hero";
+import { Input } from "@/components/ui/input";
 import { redirect } from "next/navigation";
+import { FeaturesSection } from "./features";
+import { SiteFooter } from "./site-footer";
 
 export const metadata = {
-  title: "Launchpad",
+  title: "Loggr - Time tracking made simple",
 };
 
-export default async function DashboardPage() {
+export default async function Home() {
   const user = await getCurrentUser();
   const teams = user?.workspaces;
 
   if (!teams) {
-    return (
-      <div className="container flex flex-col items-center justify-center gap-12">
-        <Card className="flex flex-col items-center justify-center space-y-4 p-12">
-          <PartyPopper className="h-12 w-12 text-primary" />
-          <h2 className="text-2xl font-bold text-primary">Thank you!</h2>
-          <p>You have successfully signed up to express your interest in our application.</p>
-          <p>Please note that our app is invite-only.</p>
-          <p>We will send you an invitation as soon as possible.</p>
-          <Link href="mailto:loggr@axioned.com" rel="noreferrer" className={buttonVariants({ size: "lg" })}>
-            Get in touch
-          </Link>
-        </Card>
-      </div>
-    );
-  }
-
-  if (teams.length === 1) {
-    redirect("/" + teams[0].slug);
+    redirect("/thanks");
   }
 
   return (
     <>
-      <div className="container flex flex-col items-center justify-center gap-12">
-        <h2>
-          Welcome back, <span>{user?.name}</span> 👋
-        </h2>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          {user?.workspaces.map((team) => (
-            <Link
-              className="hover:bg-zinc/20 flex max-w-xs flex-col gap-4 rounded-xl bg-zinc-400/10 p-4  hover:bg-zinc-400/20"
-              href={team.slug}
-              key={team.id}
-            >
-              <h3>{team.name}</h3>
-              <div className="text-lg">
-                The application lives here. Right now, its only layouts and static components.
-              </div>
-            </Link>
-          ))}
-        </div>
+      <HeroSection />
+      <FeaturesSection />
+      <div className="mx-auto flex h-[20rem] w-full max-w-2xl flex-col items-center justify-center gap-4 p-4">
+        <h1 className="text-center">Join the waitlist</h1>
+        <p>
+          We want to make time tracking and team collaboration &quot;syncing up&quot; simple. Whether you&apos;re an
+          digital agency, work with freelancers, or need to track time for your own projects, we&apos;ve got you
+          covered.
+        </p>
+        <Input type="text" placeholder="hello@loggr.dev" />
       </div>
+      <SiteFooter />
     </>
   );
 }
