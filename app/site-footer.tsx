b1ef9de9d70a16c8df1bcb61e6ebdb2bb45f8fe7
banuragaxioned <anurag@axioned.com>
2024-01-31 23:19:59 +0530

    Add site footer

---
+++ app/site-footer.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <footer className={cn(className)}>
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <p className="text-center">Built by Axioned with ❤️ from 🇮🇳</p>
+        </div>
+        <Link href={"mailto:hello@loggr.dev"} className={buttonVariants({ variant: "default", size: "sm" })}>
+          Contact
+        </Link>
+      </div>
+    </footer>
+  );
+}
