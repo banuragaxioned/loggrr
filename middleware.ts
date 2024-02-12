908502a086c1c7f844189581b18dd66a217675f5
Zishan Ansari <zishana@axioned.com>
2024-02-12 19:03:57 +0530

    fix: Marketing page auth and minor use cases + project API revamp

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api/ routes
-     * 2. /_next/ (Next.js internals)
-     * 3. /_proxy/ (special page for OG tags proxying)
-     * 4. /_static (inside /public)
-     * 5. /_vercel (Vercel internals)
-     * 6. /thanks (special page for redirecting after form submissions)
-     * 7. Static files (e.g. /favicon.ico, /sitemap.xml, /robots.txt, etc.)
-     */
-    {
-      source: "/((?!api/|_next/|_proxy/|_static|_vercel|thanks|[\\w-]+\\.\\w+).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
-  ],
+  matcher: ["/((?!api/|_next/static|_next/image|thanks|favicon.ico|^$).+)"],
 };
