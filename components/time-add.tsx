45382f58a6687d346d5f042dda52bc39c949168c
Zishan Ansari <zishana@axioned.com>
2024-02-13 13:54:46 +0530

    remove: Unused codes

--- components/time-add.tsx
+++ components/time-add.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import { CalendarIcon, CalendarPlus, Folder, List, ListPlus, Minus, Plus, Rocket } from "lucide-react";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { CalendarPlus, Folder, List, ListPlus, Minus, Plus, Rocket } from "lucide-react";
 import { toast } from "sonner";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
 import { format } from "date-fns";
 
 import { Button } from "@/components/ui/button";
@@ -22,10 +18,6 @@ import {
 } from "@/components/ui/drawer";
 
 import { cn } from "@/lib/utils";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { createTimeLog } from "@/app/_actions/create-timelog-action";
 import { ClassicDatePicker } from "./date-picker";
 
 import { Milestone, Project } from "@/types";
@@ -223,186 +215,8 @@ export function TimeAdd({ projects }: { projects: Project[] }) {
               </DrawerClose>
             </DrawerFooter>
           </form>
-          {/* <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 p-4">
-              <FormField
-                control={form.control}
-                name="date"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Date</FormLabel>
-                    <ClassicDatePicker date={date} setDate={setDate} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="projectId"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Project</FormLabel>
-                    <Popover open={projectOpen} onOpenChange={setProjectOpen}>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant="outline"
-                            role="combobox"
-                            className={cn("w-full justify-between", !field.value && "text-muted-foreground")}
-                          >
-                            {field.value
-                              ? projects.find((project) => project.value === field.value)?.label
-                              : "Select project"}
-                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-[352px] p-0">
-                        <Command>
-                          <CommandInput placeholder="Search project..." />
-                          <CommandEmpty>No project found.</CommandEmpty>
-                          <CommandGroup>
-                            {projects.map((project) => (
-                              <CommandItem
-                                value={project.label}
-                                key={project.value}
-                                onSelect={() => {
-                                  form.setValue("projectId", project.value);
-                                  setProjectOpen(false);
-                                }}
-                              >
-                                <Check
-                                  className={cn(
-                                    "mr-2 h-4 w-4",
-                                    project.value === field.value ? "opacity-100" : "opacity-0",
-                                  )}
-                                />
-                                {project.label}
-                              </CommandItem>
-                            ))}
-                          </CommandGroup>
-                        </Command>
-                      </PopoverContent>
-                    </Popover>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="milestoneId"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Milestone</FormLabel>
-                    <Popover open={milestoneOpen} onOpenChange={setMilestoneOpen}>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant="outline"
-                            role="combobox"
-                            className={cn("w-full justify-between", !field.value && "text-muted-foreground")}
-                          >
-                            {field.value
-                              ? projects.find((milestone) => milestone.value === field.value)?.label
-                              : "Select milestone"}
-                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-[352px] p-0">
-                        <Command>
-                          <CommandInput placeholder="Search milestone..." />
-                          <CommandEmpty>No milestones found.</CommandEmpty>
-                          <CommandGroup>
-                            {projects.map((milestone) => (
-                              <CommandItem
-                                value={milestone.label}
-                                key={milestone.value}
-                                onSelect={() => {
-                                  form.setValue("milestoneId", milestone.value);
-                                  setMilestoneOpen(false);
-                                }}
-                              >
-                                <Check
-                                  className={cn(
-                                    "mr-2 h-4 w-4",
-                                    milestone.value === field.value ? "opacity-100" : "opacity-0",
-                                  )}
-                                />
-                                {milestone.label}
-                              </CommandItem>
-                            ))}
-                          </CommandGroup>
-                        </Command>
-                      </PopoverContent>
-                    </Popover>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <div className="space-x-2l flex items-center justify-center">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  type="button"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => onClick(-15)}
-                  disabled={time <= 15}
-                >
-                  <Minus className="h-4 w-4" />
-                  <span className="sr-only">Decrease</span>
-                </Button>
-                <div className="flex-1 text-center">
-                  <div className="text-7xl font-bold tracking-tighter"> {time}</div>
-                  <div className="text-[0.70rem] uppercase text-muted-foreground">minutes</div>
-                </div>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  type="button"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => onClick(15)}
-                  disabled={time >= 480}
-                >
-                  <Plus className="h-4 w-4" />
-                  <span className="sr-only">Increase</span>
-                </Button>
-              </div>
-              <DrawerFooter>
-                <Button type="submit">Submit</Button>
-                <DrawerClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DrawerClose>
-              </DrawerFooter>
-            </form>
-          </Form> */}
         </div>
       </DrawerContent>
     </Drawer>
   );
 }
-
-const projects = [
-  { label: "English", value: "en" },
-  { label: "French", value: "fr" },
-  { label: "German", value: "de" },
-  { label: "Spanish", value: "es" },
-  { label: "Portuguese", value: "pt" },
-  { label: "Russian", value: "ru" },
-  { label: "Japanese", value: "ja" },
-  { label: "Korean", value: "ko" },
-  { label: "Chinese", value: "zh" },
-] as const;
-
-const FormSchema = z.object({
-  milestoneId: z.string({
-    required_error: "Please select a milestone.",
-  }),
-  date: z.date({
-    required_error: "A date is required.",
-  }),
-  projectId: z.string({
-    required_error: "A project is required.",
-  }),
-});
