d20e5078353e83e90b660b5b2d6a70c57fa8c675
banuragaxioned <anurag@axioned.com>
2024-02-15 16:47:10 +0530

    Update workflow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://plaaywright.dev/");
-
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Loggr/);
 });
 
 test("get started link", async ({ page }) => {
