7ec615296439d33bcb440d0fa17980b1ea96d204
banuragaxioned <anurag@axioned.com>
2024-02-15 16:39:34 +0530

    Cleanup (and check breakage)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("https://plaaywright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
