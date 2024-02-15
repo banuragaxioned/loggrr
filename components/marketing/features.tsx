73e4bd7170f4d8226ea1db3960ee806c02f72e1a
banuragaxioned <anurag@axioned.com>
2024-02-15 12:27:37 +0530

    Copy updates

--- components/marketing/features.tsx
+++ components/marketing/features.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import { BookOpen, Bot, LucideGanttChartSquare, Timer } from "lucide-react";
+import { BookOpen, Inbox, LucideGanttChartSquare, Scaling, Timer } from "lucide-react";
 
 export function FeaturesSection() {
   return (
@@ -24,30 +24,52 @@ const Skeleton = () => (
 const items = [
   {
     title: "Effortless time tracking",
-    description: "Track time and sync up with your team in a few clicks.",
+    description: "Track time and sync up with your team in a few clicks, and make time tracking a breeze.",
     header: <Skeleton />,
     className: "md:col-span-2",
     icon: <Timer className="h-4 w-4" />,
   },
   {
-    title: "Manage work allocation",
-    description: "Allocate work to your team and track their progress.",
+    title: "Smart suggestions",
+    description: "Get smart recommendations and quick actions.",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <span>✨</span>,
+  },
+  {
+    title: "Automated reminders",
+    description: "Get reminders to log your time, so you don't have to worry about it.",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <Inbox className="h-4 w-4" />,
+  },
+  {
+    title: "Flexibility",
+    description:
+      "Get as specific or as broad as you need to. Track strategic milestones, retainers, or tasks (if you needed to).",
+    header: <Skeleton />,
+    className: "md:col-span-2",
+    icon: <Scaling className="h-4 w-4" />,
+  },
+  {
+    title: "Simplified work allocation",
+    description: "Know what's on your plate, work to your team and track their progress.",
     header: <Skeleton />,
     className: "md:col-span-1",
     icon: <LucideGanttChartSquare className="h-4 w-4" />,
   },
   {
-    title: "Smart suggestions",
-    description: "Get recommendations based on your work allocation and previous activity.",
+    title: "Generate reports",
+    description: "Straightforward reports to help you understand and share your progress.",
     header: <Skeleton />,
     className: "md:col-span-1",
-    icon: <Bot className="h-4 w-4" />,
+    icon: <LucideGanttChartSquare className="h-4 w-4" />,
   },
   {
     title: "Designed for transparent teams",
-    description: "Built to be transparent and open by default - letting everyone know what's going on.",
+    description: "Built to be transparent and open - letting everyone know what's going on.",
     header: <Skeleton />,
-    className: "md:col-span-2",
+    className: "md:col-span-1",
     icon: <BookOpen className="h-4 w-4" />,
   },
 ];
