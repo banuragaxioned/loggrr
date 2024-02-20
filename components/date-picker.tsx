f9556c4eac0fd31ba7f0e919938473e9c2437668
Zishan Ansari <zishana@axioned.com>
2024-02-20 18:57:36 +0530

    ref: Removed unused hook

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -1,12 +1,13 @@
-import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
-import { Calendar } from "@/components/ui/calendar";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { UseFormSetValue } from "react-hook-form";
 import { addYears, format } from "date-fns";
 import { Calendar as CalendarIcon, Infinity } from "lucide-react";
 import { DateRange } from "react-day-picker";
+
+import { Calendar } from "@/components/ui/calendar";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { UseFormSetValue } from "react-hook-form";
 import { AssignFormValues } from "@/types";
 import { Checkbox } from "./ui/checkbox";
 import { GetSetDateProps } from "@/types";
