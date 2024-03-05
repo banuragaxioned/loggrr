3bbe1784e3a327880823ad5840f953dc53d302ca
Zishan Ansari <zishana@axioned.com>
2024-03-05 21:13:39 +0530

    feat: Added notebook save in localstorage

--- components/notepad.tsx
+++ components/notepad.tsx
@@ -44,6 +44,7 @@ export default function AINotepad({ notebookSubmitHandler, aiInput, setAiInput, 
                   if (aiLoading || !aiInput.trim()) return;
                   e.preventDefault();
                   notebookSubmitHandler(aiInput);
+                  localStorage.removeItem("notebook-input");
                 }}
                 className="flex flex-col gap-4"
               >
@@ -51,11 +52,13 @@ export default function AINotepad({ notebookSubmitHandler, aiInput, setAiInput, 
                   value={aiInput}
                   onChange={(e) => {
                     setAiInput(e.target.value);
+                    localStorage.setItem("notebook-input", e.target.value);
                   }}
                   onKeyDown={(e) => {
                     const ctrlKey = e.ctrlKey || e.metaKey;
                     if (ctrlKey && e.key === "Enter" && aiInput.trim()) {
                       notebookSubmitHandler(aiInput);
+                      localStorage.removeItem("notebook-input");
                     }
                   }}
                   rows={8}
