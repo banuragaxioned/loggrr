86eb1994fc90d6952d54d0e9f70c9d133f80a553
Zishan Ansari <zishana@axioned.com>
2024-03-06 14:49:34 +0530

    feat: Added a custom dismiss button

--- components/notepad-response.tsx
+++ components/notepad-response.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "./ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogClose } from "./ui/dialog";
 import { Button } from "@/components/ui/button";
 
 import NotepadCards from "./notepad-cards";
@@ -43,19 +43,35 @@ const NotepadResponse = ({ aiResponses, setAiResponses, projects, handleSubmit }
     );
   });
 
+  console.log(aiResponses, "aiResponses");
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent
         className="max-w-[calc(100%-44px)] overflow-y-auto p-0 lg:max-w-4xl"
         onEscapeKeyDown={(e) => e.preventDefault()}
         onInteractOutside={(e) => e.preventDefault()}
+        removeClose
+        tabIndex={-1}
       >
         <DialogHeader className="p-6 pb-0">
           <DialogTitle className="flex items-center justify-between">
             <div className="flex items-start gap-2">Add time entries</div>
-            <Button size="sm" className={`mr-5 ${aiResponses.length > 1 ? "visible" : "invisible"}`} disabled>
-              Submit all
-            </Button>
+            <div>
+              <Button
+                variant="outline"
+                size="sm"
+                className={`mr-2 ${aiResponses.length > 1 ? "visible" : "invisible"}`}
+                disabled
+              >
+                Submit all
+              </Button>
+              <DialogClose asChild>
+                <Button size="sm" className="hover:text-destructive">
+                  Dismiss
+                </Button>
+              </DialogClose>
+            </div>
           </DialogTitle>
           <DialogDescription className="mt-0 w-[85%] text-left">
             This is an AI-generated response and might be inaccurate. You can click on the inputs below to update the
