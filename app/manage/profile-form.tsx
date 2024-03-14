4403b705bd197444e2e91650b4dbd574daa59e0b
Zishan Ansari <zishana@axioned.com>
2024-03-14 19:32:31 +0530

    feat: Update profileform

--- app/manage/profile-form.tsx
+++ app/manage/profile-form.tsx
@@ -1,45 +1,23 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 
-const profileFormSchema = z.object({
-  name: z
-    .string()
-    .min(3, {
-      message: "Name must be at least 3 characters.",
-    })
-    .max(30, {
-      message: "Name must not be longer than 30 characters.",
-    }),
-  // timezone: z.string(),
-  // language: z.string(),
-});
-
-type ProfileFormValues = z.infer<typeof profileFormSchema>;
-
 export function ProfileForm({ user }: { user: any }) {
-  const router = useRouter();
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-  });
+  const [name, setName] = useState(user.name);
 
-  async function onSubmit(data: ProfileFormValues) {
+  async function onSubmit() {
     try {
       const response = await fetch("/api/manage/profile", {
         method: "PUT",
         body: JSON.stringify({
           id: user.id,
-          name: data.name,
+          name: name,
         }),
       });
 
@@ -54,28 +32,25 @@ export function ProfileForm({ user }: { user: any }) {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="mb-2 space-y-8" autoComplete="off">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Name" {...field} defaultValue={user.name} />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="space-y-2">
-          <Label>Email</Label>
-          <Input placeholder="email" disabled defaultValue={user.email} />
-          <p className="text-sm text-muted-foreground">You are currently using this email.</p>
-        </div>
-        {/* <FormField
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+      className="mb-2 space-y-8"
+      autoComplete="off"
+    >
+      <div className="space-y-2">
+        <Label>Name</Label>
+        <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <p className="text-sm text-muted-foreground">This is your public display name.</p>
+      </div>
+      <div className="space-y-2">
+        <Label>Email</Label>
+        <Input placeholder="Email" disabled defaultValue={user.email} />
+        <p className="text-sm text-muted-foreground">You are currently using this email.</p>
+      </div>
+      {/* <FormField
           control={form.control}
           name="timezone"
           render={({ field }) => (
@@ -125,8 +100,7 @@ export function ProfileForm({ user }: { user: any }) {
             </FormItem>
           )}
         /> */}
-        <Button type="submit">Update profile</Button>
-      </form>
-    </Form>
+      <Button type="submit">Update profile</Button>
+    </form>
   );
 }
