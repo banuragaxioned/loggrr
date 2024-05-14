e5aceb8f2a94e7947ce8dfcf5450e3d19de68692
Zishan Ansari <zishana@axioned.com>
2024-05-14 19:28:04 +0530

    fix: Minor alignment issue

--- app/[team]/projects/[project]/teams-card.tsx
+++ app/[team]/projects/[project]/teams-card.tsx
@@ -70,7 +70,7 @@ export const TeamsCard = ({
             // rotate: rotate,
             whiteSpace: "nowrap",
           }}
-          className="absolute -left-1/2 -top-16 z-50 flex translate-x-1/2  flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+          className="absolute -left-[110%] -top-16 z-50 flex translate-x-1/2  flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
         >
           <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent " />
           <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent " />
