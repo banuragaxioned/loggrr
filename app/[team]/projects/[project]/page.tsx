b1d4680e7911ff190a66401e06afe366f304db0f
Zishan Ansari <zishana@axioned.com>
2024-05-14 15:44:17 +0530

    feat: Added time card

--- app/[team]/projects/[project]/page.tsx
+++ app/[team]/projects/[project]/page.tsx
@@ -1,99 +1,59 @@
-import { Hourglass, Users } from "lucide-react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { DashboardShell } from "@/components/ui/shell";
+import { notFound } from "next/navigation";
+
 import { db } from "@/server/db";
-import { getTimeInHours } from "@/lib/helper";
+
+import { DashboardShell } from "@/components/ui/shell";
 import { pageProps } from "@/types";
-import { teamMemberStats as getTeamMemberStats } from "@/server/services/project";
+import TimeLoggedCard from "./time-logged";
 
 export default async function Page({ params }: pageProps) {
   const { team, project } = params;
 
   if (!project) {
-    return null;
+    return notFound();
   }
 
-  const projectDetails = await db.project.findUnique({
+  const timeLogOverall = await db.timeEntry.groupBy({
+    by: ["projectId"],
     where: {
-      id: +project,
       workspace: {
         slug: team,
       },
+      projectId: +project,
+    },
+    _sum: {
+      time: true,
     },
   });
 
-  if (!projectDetails) {
-    return null;
-  }
-
-  const timeLogSummary = await db.timeEntry.groupBy({
-    by: ["milestoneId"],
+  const timeLogLast30 = await db.timeEntry.groupBy({
+    by: ["projectId"],
     where: {
       workspace: {
         slug: team,
       },
       projectId: +project,
       date: {
-        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+        gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
       },
     },
     _sum: {
       time: true,
     },
-    _count: {
-      time: true,
-    },
-    _avg: { time: true },
   });
 
-  // flatten the result
-  const timeLogSummaryFlat = timeLogSummary.map((item) => {
-    return {
-      id: item.milestoneId,
-      time: item._sum.time,
-      count: item._count.time,
-      avg: item._avg.time,
-    };
-  });
-
-  const teamMemberStats = await getTeamMemberStats(team, project);
-
-  const percentageChange = (current: number, previous: number) => {
-    // return + and - sign with percentage change as string
-    if (current === 0 || previous === 0) return "0%";
-    const percentage = ((current - previous) / previous) * 100;
-    return percentage > 0 ? `+${percentage.toFixed(2)}%` : `${percentage.toFixed(2)}%`;
+  const timecardProp = {
+    overall: timeLogOverall[0]?._sum.time ?? 0,
+    last30: timeLogLast30[0]?._sum.time ?? 0,
   };
 
   return (
     <DashboardShell>
-      {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total time spent</CardTitle>
-            <Hourglass className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{getTimeInHours(timeLogSummaryFlat[0]?.time ?? 0)}h</div>
-            <p className="text-xs text-muted-foreground">out of {projectDetails.budget} budgeted hours</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Active contributors in 30 days</CardTitle>
-            <Users className="h-4 w-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{teamMemberStats[0].length}</div>
-            <p className="text-xs text-muted-foreground">
-              {percentageChange(teamMemberStats[0].length, teamMemberStats[1].length)} from last month
-            </p>
-          </CardContent>
-        </Card>
-      </div> */}
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-9 grid h-[400px] place-items-center border">Chart area</div>
-        <div className="col-span-3 border">2</div>
+        <div className="col-span-3">
+          <TimeLoggedCard timecardProp={timecardProp} />
+        </div>
       </div>
     </DashboardShell>
   );
---
+++ app/[team]/projects/[project]/time-logged.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { getTimeInHours } from "@/lib/helper";
+import { Hourglass } from "lucide-react";
+import React from "react";
+
+type TimeLoggedCardProps = {
+  timecardProp: {
+    overall: number;
+    last30: number;
+  };
+};
+
+const TimeLoggedCard = ({ timecardProp }: TimeLoggedCardProps) => {
+  return (
+    <Card className="p-4 shadow-none">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 p-0 text-muted-foreground">
+        <p className="text-lg font-semibold">Time logged</p>
+        <Hourglass size={16} className="mt-0" />
+      </CardHeader>
+      <CardContent className="mt-2 space-y-1.5 p-0">
+        <p className="text-3xl font-semibold">
+          {getTimeInHours(timecardProp.overall)}
+          <span className="text-xl">h</span>
+        </p>
+        <p className="flex items-center gap-1">
+          {getTimeInHours(timecardProp.last30)}h <span className="text-sm text-muted-foreground">(last 30 days)</span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TimeLoggedCard;
