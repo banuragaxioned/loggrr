ee65475821c476a75349f3db5a9395eea900458b
Zishan Ansari <zishana@axioned.com>
2024-05-15 20:46:41 +0530

    feat: Populate last 30 Days

--- app/[team]/projects/[project]/page.tsx
+++ app/[team]/projects/[project]/page.tsx
@@ -9,8 +9,6 @@ import TimeLoggedCard from "./timelogged-card";
 import { TeamsCard } from "./teams-card";
 import BillableCard from "./billable-card";
 import TimeChart from "./time-chart";
-import { format } from "date-fns";
-import { getTimeInHours } from "@/lib/helper";
 
 export const metadata: Metadata = {
   title: `Overview`,
@@ -119,7 +117,7 @@ export default async function Page({ params }: pageProps) {
     },
   });
 
-  // get all last 14 days timeentries for the project group by date
+  // get all last 30 days timeentries for the project group by date
   const timeEntries = await db.timeEntry.groupBy({
     by: ["date"],
     where: {
@@ -136,16 +134,39 @@ export default async function Page({ params }: pageProps) {
     },
   });
 
+  // get all last 30 days timeentries for the project group by date
+  const billableTimeEntries = await db.timeEntry.groupBy({
+    by: ["date"],
+    where: {
+      workspace: {
+        slug: team,
+      },
+      projectId: +project,
+      date: {
+        gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+      },
+      billable: true,
+    },
+    _sum: {
+      time: true,
+    },
+  });
+
   const formattedEntries = timeEntries.map((entry) => ({
     date: entry.date,
-    time: +getTimeInHours(entry._sum.time ?? 0),
+    time: entry._sum.time ?? 0,
+  }));
+
+  const formattedBillableEntries = billableTimeEntries.map((entry) => ({
+    date: entry.date,
+    time: entry._sum.time ?? 0,
   }));
 
   return (
     <DashboardShell>
       <div className="grid grid-cols-12 gap-4">
         <div className="col-span-12 lg:col-span-9">
-          <TimeChart timeEntries={formattedEntries} />
+          <TimeChart timeEntries={formattedEntries} billableEntries={formattedBillableEntries} />
         </div>
         <div className="col-span-12 flex flex-col gap-4 lg:col-span-3">
           <TeamsCard items={allMembers} activeUserCount={userActivity.length} />
--- app/[team]/projects/[project]/time-chart.tsx
+++ app/[team]/projects/[project]/time-chart.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { Card, CardHeader } from "@/components/ui/card";
-import { format } from "date-fns";
+import { format, startOfToday, subDays } from "date-fns";
 import { Info } from "lucide-react";
 import React from "react";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
+type TimeChartProps = {
+  timeEntries: { date: Date; time: number }[];
+  billableEntries: { date: Date; time: number }[];
+};
 
 const CustomTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
@@ -12,13 +17,61 @@ const CustomTooltip = ({ active, payload, label }: any) => {
       <div className="rounded-md border border-border bg-primary-foreground p-2 text-xs shadow-sm">
         <p className="label">{format(label, "EEE, dd MMM, yyyy")}</p>
         <p className="desc">Hours logged: {payload[0].value}h</p>
+        {/* <p className="desc">Billable: {payload[1].value}h</p> */}
       </div>
     );
   }
 };
 
-const TimeChart = ({ timeEntries }: { timeEntries: { date: Date; time: number }[] }) => {
+const TimeChart = ({ timeEntries, billableEntries }: TimeChartProps) => {
   const formatXAxis = (tickItem: Date) => format(tickItem, "MMM dd");
+
+  const [data, setData] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    const getLast30Days = () => {
+      const today = startOfToday();
+      const last30Days = [];
+      for (let i = 29; i >= 0; i--) {
+        const currentDate = subDays(today, i);
+        last30Days.push(format(currentDate, "yyyy-MM-dd"));
+      }
+
+      return last30Days;
+    };
+
+    const last30Days = getLast30Days();
+    const transformedData: any = {};
+
+    timeEntries.forEach((entry: any, i) => {
+      const date = format(new Date(entry.date), "yyyy-MM-dd");
+      transformedData[date] = { ...transformedData[date], time: entry.time / 60 || 0 };
+    });
+
+    billableEntries.forEach((entry: any, i) => {
+      const date = format(new Date(entry.date), "yyyy-MM-dd");
+      transformedData[date] = { ...transformedData[date], billable: entry.time / 60 || 0 };
+    });
+
+    // Fill in missing dates with time: 0
+    last30Days.forEach((date) => {
+      if (!transformedData[date]?.time) {
+        transformedData[date] = { ...transformedData[date], time: 0 };
+      }
+      if (!transformedData[date]?.billable) {
+        transformedData[date] = { ...transformedData[date], billable: 0 };
+      }
+    });
+
+    // Convert transformed data back to an array and sort
+    const finalData = last30Days.map((date) => ({
+      time: transformedData[date].time,
+      billable: transformedData[date].billable,
+      date,
+    }));
+
+    setData(finalData);
+  }, [timeEntries, billableEntries]);
 
   return (
     <Card className="select-none p-0 shadow-none">
@@ -31,7 +84,7 @@ const TimeChart = ({ timeEntries }: { timeEntries: { date: Date; time: number }[
       </CardHeader>
       <div className="flex h-[200px] items-end justify-end py-2 pr-8 sm:h-[300px] md:h-[446px]">
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart width={500} height={300} data={timeEntries}>
+          <LineChart width={500} height={300} data={data}>
             <XAxis
               dataKey="date"
               tick={{ fontSize: 12 }}
@@ -39,7 +92,7 @@ const TimeChart = ({ timeEntries }: { timeEntries: { date: Date; time: number }[
               interval="equidistantPreserveStart"
             />
             <YAxis tick={{ fontSize: 12 }} />
-            <Tooltip content={<CustomTooltip />} cursor />
+            <Tooltip content={<CustomTooltip />} cursor={false} />
             <Line
               type="monotone"
               dataKey="time"
@@ -47,6 +100,13 @@ const TimeChart = ({ timeEntries }: { timeEntries: { date: Date; time: number }[
               dot={{ fill: "hsl(var(--muted-foreground))", strokeWidth: 1, r: 3 }}
               activeDot={{ fill: "hsl(var(--secondary))", stroke: "hsl(var(--muted-foreground))", r: 4 }}
             />
+            {/* <Line
+              type="monotone"
+              dataKey="billable"
+              style={{ stroke: "hsl(var(--success))" }}
+              dot={{ fill: "hsl(var(--muted-foreground))", strokeWidth: 1, r: 3 }}
+              activeDot={{ fill: "hsl(var(--secondary))", stroke: "hsl(var(--muted-foreground))", r: 4 }}
+            /> */}
           </LineChart>
         </ResponsiveContainer>
       </div>
