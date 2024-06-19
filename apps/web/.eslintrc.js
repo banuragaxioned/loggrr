95d4b1dc8277fe6c55ed7394e42bf78ce6c48721
Anurag Banerjee <anurag@axioned.com>
2024-06-19 13:40:47 +0530

    WIP changes

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,19 +1,4 @@
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   extends: ["@loggrr/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "tailwindcss/no-custom-classname": "off",
-  },
-  settings: {
-    tailwindcss: {
-      callees: ["cn"],
-    },
-  },
 };
