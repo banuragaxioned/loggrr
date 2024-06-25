763cbc34416033e9854507ab0ed1dea5f4289b84
Zishan Ansari <zishana@axioned.com>
2024-06-25 17:04:57 +0530

    feat: Added ability to archive tasks

--- app/[team]/projects/[project]/(projects)/tasks/task-data.tsx
+++ app/[team]/projects/[project]/(projects)/tasks/task-data.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Edit, Trash, Hourglass, ClipboardCheck, Archive, Activity } from "lucide-react";
 import { Badge } from "@tremor/react";
 import { toast } from "sonner";
@@ -21,14 +21,17 @@ import {
 
 import { ProjectTaskForm } from "@/components/forms/projectTaskForm";
 import { updateTaskStatus } from "@/app/_actions/update-status";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+interface ListProps {
+  id: number;
+  name: string;
+  budget: number | null;
+  status: string;
+}
 
 export interface TaskDataProps {
-  taskList: {
-    id: number;
-    name: string;
-    budget: number | null;
-    status: string;
-  }[];
+  taskList: ListProps[];
   team: string;
   project: number;
 }
@@ -41,7 +44,6 @@ export interface EditReferenceObj {
 
 const TaskData = ({ taskList, team, project }: TaskDataProps) => {
   const router = useRouter();
-  const params = useParams();
   const [edit, setEdit] = useState<EditReferenceObj>({ obj: {}, isEditing: false, id: null });
   const [isFormOpen, setIsFormOpen] = useState(false);
 
@@ -76,6 +78,82 @@ const TaskData = ({ taskList, team, project }: TaskDataProps) => {
     }
   };
 
+  const archivedTasks = taskList.filter((task) => task.status === "ARCHIVED");
+  const publishedTasks = taskList.filter((task) => task.status === "PUBLISHED");
+
+  const List = (data: ListProps[]) => (
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+      {data.map((item, index) => {
+        const tempObj = {
+          ...item,
+          name: item.name,
+          budget: item.budget,
+          status: item.status,
+        };
+
+        const statusToUpdate = item.status === "PUBLISHED" ? "ARCHIVED" : "PUBLISHED";
+
+        const updateTask = async () => {
+          const response = await updateTaskStatus(team, +project, item.id, statusToUpdate);
+          if (response) {
+            toast.message(`Task ${statusToUpdate === "PUBLISHED" ? "unarchived" : "archived"}`);
+            router.refresh();
+          }
+        };
+
+        return (
+          <Card key={index} className="group flex justify-between rounded-md border border-border p-3 shadow-none">
+            <div className="flex items-center justify-start space-x-5">
+              <div className="flex gap-2">
+                {item?.budget !== null && item.budget > 0 && <Badge icon={Hourglass}>{item?.budget}</Badge>}
+                <p className="text-sm font-medium">{item?.name}</p>
+              </div>
+            </div>
+            <div className="invisible flex gap-1 group-hover:visible">
+              <button onClick={updateTask} title="Archive" className="p-1 hover:opacity-75">
+                {item.status === "PUBLISHED" ? <Archive size={16} /> : <Activity size={16} />}
+              </button>
+              <button
+                onClick={() => {
+                  setIsFormOpen(true);
+                  editEntryHandler(tempObj, item.id);
+                }}
+                title="Edit"
+                className="p-1 hover:opacity-75"
+              >
+                <Edit size={16} />
+              </button>
+
+              <Dialog>
+                <DialogTrigger asChild>
+                  <button title="Delete" className="p-1 hover:opacity-75">
+                    <Trash size={16} className="text-destructive" />
+                  </button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Are you sure to delete this task?</DialogTitle>
+                    <DialogDescription>
+                      This action cannot be undone. This will permanently delete your task.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <Button type="button" variant="outline" size="sm" asChild>
+                      <DialogClose>Cancel</DialogClose>
+                    </Button>
+                    <Button type="button" size="sm" onClick={() => deleteTask(item.id)} asChild>
+                      <DialogClose>Delete</DialogClose>
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </Card>
+        );
+      })}
+    </div>
+  );
+
   return (
     <>
       <ProjectTaskForm
@@ -87,75 +165,27 @@ const TaskData = ({ taskList, team, project }: TaskDataProps) => {
         setIsFormOpen={setIsFormOpen}
       />
       {Array.isArray(taskList) && taskList.length > 0 ? (
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          {taskList.map((item, index) => {
-            const tempObj = {
-              ...item,
-              name: item.name,
-              budget: item.budget,
-              status: item.status,
-            };
-
-            const statusToUpdate = item.status === "PUBLISHED" ? "ARCHIVED" : "PUBLISHED";
-
-            const updateTask = async () => {
-              const response = await updateTaskStatus(params.team as string, +params.project, item.id, statusToUpdate);
-              if (response) {
-                toast.message(`Task ${statusToUpdate === "PUBLISHED" ? "unarchived" : "archived"}`);
-                router.refresh();
-              }
-            };
-
-            return (
-              <Card key={index} className="group flex justify-between rounded-md border border-border p-3 shadow-none">
-                <div className="flex items-center justify-start space-x-5">
-                  <div className="flex gap-2">
-                    {item?.budget !== null && item.budget > 0 && <Badge icon={Hourglass}>{item?.budget}</Badge>}
-                    <p className="text-sm font-medium">{item?.name}</p>
-                  </div>
-                </div>
-                <div className="invisible flex gap-1 group-hover:visible">
-                  <button onClick={updateTask} title="Archive" className="p-1 hover:opacity-75">
-                    {item.status === "PUBLISHED" ? <Archive size={16} /> : <Activity size={16} />}
-                  </button>
-                  <button
-                    onClick={() => {
-                      setIsFormOpen(true);
-                      editEntryHandler(tempObj, item.id);
-                    }}
-                    title="Edit"
-                    className="p-1 hover:opacity-75"
-                  >
-                    <Edit size={16} />
-                  </button>
-
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <button title="Delete" className="p-1 hover:opacity-75">
-                        <Trash size={16} className="text-destructive" />
-                      </button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                      <DialogHeader>
-                        <DialogTitle>Are you sure to delete this task?</DialogTitle>
-                        <DialogDescription>
-                          This action cannot be undone. This will permanently delete your task.
-                        </DialogDescription>
-                      </DialogHeader>
-                      <DialogFooter>
-                        <Button type="button" variant="outline" size="sm" asChild>
-                          <DialogClose>Cancel</DialogClose>
-                        </Button>
-                        <Button type="button" size="sm" onClick={() => deleteTask(item.id)} asChild>
-                          <DialogClose>Delete</DialogClose>
-                        </Button>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              </Card>
-            );
-          })}
+        <div className="flex flex-col gap-4">
+          <Accordion type="single" collapsible className="w-full" defaultValue="published">
+            <AccordionItem value="published" className="rounded-xl border px-4">
+              <AccordionTrigger className="text-base font-normal tracking-normal hover:no-underline">
+                Published task
+              </AccordionTrigger>
+              <AccordionContent>
+                {publishedTasks.length > 0 ? List(publishedTasks) : <p className="py-3.5">No published task found!</p>}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="archived" className="rounded-xl border px-4">
+              <AccordionTrigger className="text-base font-normal tracking-normal hover:no-underline">
+                Archived task
+              </AccordionTrigger>
+              <AccordionContent>
+                {archivedTasks.length > 0 ? List(archivedTasks) : <p className="py-3.5">No archived task found!</p>}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       ) : (
         <Card className="flex h-[200px] flex-col items-center justify-center space-y-2 p-11 text-center shadow-none lg:h-[414px]">
