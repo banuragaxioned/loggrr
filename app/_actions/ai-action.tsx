c8de6db4f8277667391330867270f04c7bfe7579
Anurag Banerjee <anurag@axioned.com>
2024-07-05 11:57:21 +0530

    Allow users to all and billable data

--- app/_actions/ai-action.tsx
+++ app/_actions/ai-action.tsx
@@ -5,7 +5,8 @@ import { openai } from "@ai-sdk/openai";
 import { ReactNode } from "react";
 import { z } from "zod";
 import { generateId } from "ai";
-import { TimeEntries } from "@/components/ai/logged";
+import { ShowTimeEntries, ShowTimeEntriesSkeleton } from "@/components/ai/logged";
+import { getCurrentUser } from "@/server/session";
 
 export interface ServerMessage {
   role: "user" | "assistant";
@@ -22,10 +23,15 @@ export async function continueConversation(input: string): Promise<ClientMessage
   "use server";
 
   const history = getMutableAIState();
+  const user = await getCurrentUser();
+  const workspace = "axioned";
 
   const result = await streamUI({
     model: openai("gpt-4o"),
-    system: `You help users understand their time entries. ` + `For context, today's date is ${new Date()}. `,
+    system:
+      `You help users understand their time entries for the workspace ${workspace}. ` +
+      `For context, today's date is ${new Date()} and the current user is ${user?.name} and their userId is ${user?.id}.`,
+    // `They can't ask for other's data.`
     messages: [...history.get(), { role: "user", content: input }],
     text: ({ content, done }) => {
       if (done) {
@@ -35,20 +41,29 @@ export async function continueConversation(input: string): Promise<ClientMessage
       return <div>{content}</div>;
     },
     tools: {
-      // get today's date
-      // getDate: {
-      //   description: "Get today's date",
-      //   parameters: z.object({
-      //     format: z.string().transform((str) => new Date(str)),
-      //   }),
-      //   generate: () => {
-      //     return new Date().toString();
-      //   },
-      // },
+      lookupFlight: {
+        description: "lookup details for a flight",
+        parameters: z.object({
+          flightNumber: z.string().describe("The flight number"),
+        }),
+        generate: async function* ({ flightNumber }) {
+          yield `Looking up details for flight ${flightNumber}...`;
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+
+          const details = { flightNumber, departureTime: "10:00", arrivalTime: "12:00" };
+          return (
+            <div>
+              <div>Flight Number: {details.flightNumber}</div>
+              <div>Departure Time: {details.departureTime}</div>
+              <div>Arrival Time: {details.arrivalTime}</div>
+            </div>
+          );
+        },
+      },
       showLoggedTime: {
         description: "Get the total time logged for the user",
         parameters: z.object({
-          userId: z.number().describe("The user ID to get the total time logged for"),
+          workspace: z.string().min(1).describe("The workspace slug to get the time logged for"),
           startDate: z
             .string()
             .transform((str) => new Date(str))
@@ -57,16 +72,30 @@ export async function continueConversation(input: string): Promise<ClientMessage
             .string()
             .transform((str) => new Date(str))
             .describe("The end date to get the time logged for"),
+          userId: z.number().optional().describe("The user ID to get the total time logged for"),
+          billable: z
+            .boolean()
+            .optional()
+            .describe("True for billable time, false for non-billable time. Defaults to all time."),
         }),
-        generate: async ({ userId, startDate, endDate }) => {
-          history.done((messages: ServerMessage[]) => [
+        generate: async function* ({ workspace, userId, startDate, endDate, billable }) {
+          yield <ShowTimeEntriesSkeleton />;
+          history.done((messages: ClientMessage[]) => [
             ...messages,
             {
               role: "assistant",
-              content: `Showing logged time for for ${userId} between ${startDate} and ${endDate}`,
+              content: `Showing billable logged time for for ${userId} between ${startDate} and ${endDate} on the workspace ${workspace}.`,
             },
           ]);
-          return <TimeEntries userId={userId} startDate={startDate} endDate={endDate} />;
+          return (
+            <ShowTimeEntries
+              workspace={workspace}
+              userId={userId}
+              startDate={startDate}
+              endDate={endDate}
+              billable={billable}
+            />
+          );
         },
       },
     },
--- server/services/ai.ts
+++ server/services/ai.ts
@@ -1,21 +1,24 @@
 import { db } from "@/server/db";
-import { endOfDay, startOfDay, subDays } from "date-fns";
+import { endOfDay, startOfDay } from "date-fns";
 
 // get time logged by the user for a specific date range
-export const getLoggedTime = async (slug: string, userId: number, startDate: Date, endDate: Date) => {
+export const getLoggedTime = async (
+  slug: string,
+  startDate: Date,
+  endDate: Date,
+  userId?: number,
+  billable?: boolean,
+) => {
   const start = startOfDay(startDate);
   const end = endOfDay(endDate);
 
-  const response = await db.timeEntry.groupBy({
-    by: ["projectId"],
-    orderBy: {
-      projectId: "asc",
-    },
+  const response = await db.timeEntry.aggregate({
     where: {
       userId,
       workspace: {
         slug,
       },
+      billable,
       date: {
         gte: start,
         lte: end,
