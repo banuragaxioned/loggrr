df770d903db2ae969191d458aa2779496d0d3ad0
Anurag Banerjee <anurag@axioned.com>
2024-09-08 12:35:53 +0530

    Add middleware parser util

---
+++ lib/middleware/utils/parse.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from "next/server";
+
+export const parse = (req: NextRequest) => {
+  let domain = req.headers.get("host") as string;
+  // remove www. from domain and convert to lowercase
+  domain = domain.replace("www.", "").toLowerCase();
+
+  // path is the path of the URL (e.g. dub.sh/stats/github -> /stats/github)
+  let path = req.nextUrl.pathname;
+
+  // fullPath is the full URL path (along with search params)
+  const searchParams = req.nextUrl.searchParams.toString();
+  const searchParamsString = searchParams.length > 0 ? `?${searchParams}` : "";
+  const fullPath = `${path}${searchParamsString}`;
+
+  // Here, we are using decodeURIComponent to handle foreign languages like Hebrew
+  const key = decodeURIComponent(path.split("/")[1]); // key is the first part of the path (e.g. dub.sh/stats/github -> stats)
+  const fullKey = decodeURIComponent(path.slice(1)); // fullKey is the full path without the first slash (to account for multi-level subpaths, e.g. d.to/github/repo -> github/repo)
+
+  return { domain, path, fullPath, key, fullKey, searchParamsString };
+};
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,32 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { parse } from "./lib/middleware/utils";
+
+export default withAuth(
+  function middleware(req) {
+    const { key } = parse(req);
+    console.log("Key:", key);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        console.log("Auth callback triggered, token present:", !!token);
+        return !!token;
+      },
+    },
+  },
+);
 
 export const config = {
-  matcher: ["/((?!api/|_next/static|_next/image|thanks|favicon.ico|favicon.svg|^$).+)"],
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api/ routes
+     * 2. /_next/ (Next.js internals)
+     * 3. /_proxy/ (proxies for third-party services)
+     * 4. /_static (inside /public)
+     * 5. /_vercel (Vercel internals)
+     * 6. Static files (e.g. /favicon.ico, /sitemap.xml, /robots.txt, etc.)
+     */
+    "/((?!api/|_next/static|_next/image|thanks|favicon.ico|favicon.svg|^$).+)",
+  ],
 };
