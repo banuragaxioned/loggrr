cc0ffd9faa8372e54899e20636c5d719aae313ee
Zishan Ansari <zishana@axioned.com>
2024-10-23 12:51:32 +0530

    Revert "Add export CSV on project details page (#547)"
    This reverts commit a5d265f5f551e1efaf65c380ff8c04c49fe2165f.

--- app/[team]/projects/[project]/components/toolbar.tsx
+++ app/[team]/projects/[project]/components/toolbar.tsx
@@ -3,11 +3,12 @@
 import { useCallback, useState } from "react";
 import Link from "next/link";
 import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation";
-import { format, startOfDay, startOfToday, subDays } from "date-fns";
+import { format, startOfDay, startOfMonth, startOfToday, subDays } from "date-fns";
 import { CircleDollarSign, Download, ListRestart, Loader2, Users } from "lucide-react";
 import csvDownload from "json-to-csv-export";
 
 import { cn } from "@/lib/utils";
+import { DataTableToolbarProps } from "@/types";
 import useLocale from "@/hooks/useLocale";
 
 import { Button } from "@/components/ui/button";
@@ -35,8 +36,6 @@ export function DataTableToolbar({
   const selectedBilling = searchParams.get("billable");
   const selectedMembers = searchParams.get("members");
 
-  const selectedProject = pathname.includes("projects") ? pathname.split("/")[3] : null;
-
   const peopleFilter = {
     title: "Members",
     searchable: true,
@@ -62,7 +61,6 @@ export function DataTableToolbar({
           selectedRange,
           selectedBilling,
           selectedMembers,
-          selectedProject,
         }),
       });
 
@@ -71,9 +69,8 @@ export function DataTableToolbar({
       }
 
       const data = await response.json();
-
       const currentTime = format(new Date(), "dd-MM-yyyy (hh﹕mm a)");
-      const filename = `Project Report ${data[0]?.project} ${currentTime}.csv`;
+      const filename = `Logged Report ${currentTime}.csv`;
 
       const dataToConvert = {
         data,
@@ -82,7 +79,7 @@ export function DataTableToolbar({
         headers: ["Client", "Project", "User", "Category", "Task", "Date", "Comment", "Time logged", "Billing type"],
       };
 
-      csvDownload(dataToConvert);
+      // csvDownload(dataToConvert);
     } catch (error) {
       console.error("There was a problem with your fetch operation:", error);
     } finally {
@@ -168,7 +165,7 @@ export function DataTableToolbar({
         </li>
       </ul>
       {/* Right Area */}
-      <div className="no-print flex flex-wrap items-center justify-end gap-2">
+      {/* <div className="no-print flex flex-wrap items-center justify-end gap-2">
         <CustomTooltip
           trigger={
             <Button
@@ -183,7 +180,7 @@ export function DataTableToolbar({
           }
           content="Export CSV"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
