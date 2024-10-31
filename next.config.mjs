596194ac92838d865f44158cbffa21f150c3169e
Anurag Banerjee <anurag@axioned.com>
2024-10-31 09:07:44 +0530

    Update matcher pattern

--- next.config.mjs
+++ next.config.mjs
@@ -23,11 +23,15 @@ const config = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/:path*",
         headers: [
           {
             key: "X-Frame-Options",
             value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none'",
           },
         ],
       },
