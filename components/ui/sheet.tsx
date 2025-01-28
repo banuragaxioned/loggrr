9a545bd7400b533b991d47b2b3e0c0de2678ec54
Anurag Banerjee <anurag@axioned.com>
2025-01-28 23:55:20 +0530

    Update shadcn components (#559)
    * Update shadcn components
    
    * Add sidebar
    
    * Create use-mobile.tsx
    
    * Remove mr-2 from faceted-filter
    
    * Hide WIP Notifications menu

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -6,7 +6,6 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "./button";
 
 const Sheet = SheetPrimitive.Root;
 
@@ -60,12 +59,8 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
       <SheetOverlay />
       <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
         {children}
-        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <Button variant="outline" size="icon" asChild>
-            <div>
-              <X size={22} />
-            </div>
-          </Button>
+        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <X className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
