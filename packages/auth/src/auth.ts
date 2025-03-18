f16130dfa813e1d60e38338a437e18fdbe19efb3
Anurag Banerjee <anurag@axioned.com>
2025-03-18 11:00:05 +0530

    feat: add cookie caching, openapi docs for better-auth

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,15 +1,22 @@
+import { session } from "./../../db/src/schemas/auth-schema";
 import type { BetterAuthOptions } from "better-auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { admin, organization } from "better-auth/plugins";
+import { admin, organization, openAPI } from "better-auth/plugins";
 import { db } from "@workspace/db/client";
 
 export const config = {
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  plugins: [admin(), organization()],
+  plugins: [admin(), organization(), openAPI({})],
   secret: process.env.BETTER_AUTH_SECRET!,
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60, // Cache duration in seconds
+    },
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
