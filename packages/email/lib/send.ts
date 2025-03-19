31f565179a5b61140b4a91b68e9358dc8f945cfa
Anurag Banerjee <anurag@axioned.com>
2025-03-19 12:04:55 +0530

    fix: test email delivery failure

--- packages/email/lib/send.ts
+++ packages/email/lib/send.ts
@@ -14,7 +14,7 @@ interface SendEmailProps {
 const transporter = nodemailer.createTransport({
   host: env.EMAIL_HOST,
   port: env.EMAIL_PORT,
-  secure: env.NODE_ENV === "production",
+  secure: false,
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_PASSWORD,
