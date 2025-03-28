362df021ffe7f23c97ca5a0fd4ae9f00fd8b02f1
Anurag Banerjee <anurag@axioned.com>
2025-03-29 00:38:14 +0530

    Update server and client

--- apps/web/trpc/server.tsx
+++ apps/web/trpc/server.tsx
@@ -1,4 +1,5 @@
 import "server-only"; // <-- ensure this file cannot be imported from the client
+
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import { cache } from "react";
 import { createTRPCContext } from "./init";
