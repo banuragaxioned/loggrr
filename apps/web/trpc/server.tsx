c51f4e86c479af56ca5a4d9c125fe96fd571c364
Anurag Banerjee <anurag@axioned.com>
2025-03-28 22:11:36 +0530

    Update server.tsx

--- apps/web/trpc/server.tsx
+++ apps/web/trpc/server.tsx
@@ -4,23 +4,14 @@ import { cache } from "react";
 import { createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
-import { createTRPCClient, httpLink } from "@trpc/client";
 
-// IMPORTANT: Create a stable getter for the query client that
-//            will return the same client during the same request.
+// IMPORTANT: Create a stable getter for the query client that will return the same client during the same request.
+
 export const getQueryClient = cache(makeQueryClient);
 
 export const trpc = createTRPCOptionsProxy({
   ctx: createTRPCContext,
   router: appRouter,
-  queryClient: getQueryClient,
-});
-
-// If your router is on a separate server, pass a client:
-createTRPCOptionsProxy({
-  client: createTRPCClient({
-    links: [httpLink({ url: "..." })],
-  }),
   queryClient: getQueryClient,
 });
 
