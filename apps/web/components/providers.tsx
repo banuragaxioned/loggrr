8431e6643db81371dfb3967096b357aa2a6734e2
Anurag Banerjee <anurag@axioned.com>
2025-03-28 21:22:37 +0530

    feat: complete basic installation

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -6,24 +6,27 @@ import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@workspace/ui/components/sonner";
+import { TRPCProvider } from "@/trpc/client";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme>
-      <NuqsAdapter>
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools />
-          <Toaster />
-        </QueryClientProvider>
-      </NuqsAdapter>
-    </NextThemesProvider>
+    <TRPCProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        enableColorScheme>
+        <NuqsAdapter>
+          <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools />
+            <Toaster />
+          </QueryClientProvider>
+        </NuqsAdapter>
+      </NextThemesProvider>
+    </TRPCProvider>
   );
 }
---
+++ apps/web/trpc/client.tsx
@@ -0,0 +1,29 @@
+"use client";
+// ^-- to make sure we can mount the Provider from a server component
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import React, { useState } from "react";
+import { appRouter } from "./routers/_app";
+import { createTRPCContext } from "@trpc/tanstack-react-query";
+
+const { TRPCProvider: TRPCProviderBase } = createTRPCContext();
+
+export function TRPCProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    createTRPCClient({
+      links: [
+        httpBatchLink({
+          url: "/api/trpc",
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <TRPCProviderBase trpcClient={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </TRPCProviderBase>
+  );
+}
