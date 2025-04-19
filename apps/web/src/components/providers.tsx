bfdb2fbc2d38f31bba152e679d3fbd1f36a02adb
Anurag Banerjee <anurag@axioned.com>
2025-04-19 18:45:49 +0530

    feat: add nuqs

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { queryClient } from "@/utils/trpc";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
@@ -7,7 +8,9 @@ import { Toaster } from "./ui/sonner";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </QueryClientProvider>
       <Toaster richColors />
     </ThemeProvider>
   );
