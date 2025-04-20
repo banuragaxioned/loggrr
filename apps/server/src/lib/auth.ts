f6ddc39a9d9432939c9f14655e4a30be1f83d1b4
Anurag Banerjee <anurag@axioned.com>
2025-04-20 21:34:57 +0530

    feat: add google redirectURI

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -15,6 +15,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI: process.env.CORS_ORIGIN + "/api/auth/callback/google",
     },
   },
   plugins: [openAPI(), admin(), organization()],
