4e5d1922a9e3d6098ba7f0737b0ea31fe1e90b0a
Anurag Banerjee <anurag@axioned.com>
2025-04-25 13:49:13 +0530

    refactor: tweak db connection config

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,4 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
