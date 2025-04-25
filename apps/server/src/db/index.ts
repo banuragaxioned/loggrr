cc4a156f8d85c245db8a4546a78513834eb6edc3
Anurag Banerjee <anurag@axioned.com>
2025-04-25 15:56:11 +0530

    fix: remove ts alias (causing vercel build failure)

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,4 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
