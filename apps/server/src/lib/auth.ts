2cfc6f73082002823b06cd114d885a2035e9efc3
Anurag Banerjee <anurag@axioned.com>
2025-04-25 14:08:19 +0530

    chore(deps): package update

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -3,21 +3,20 @@ import { openAPI, admin, organization } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
-import { env } from "process";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: schema,
   }),
-  baseURL: env.BETTER_AUTH_URL,
-  trustedOrigins: [env.CORS_ORIGIN!],
+  baseURL: process.env.BETTER_AUTH_URL!,
+  trustedOrigins: [process.env.CORS_ORIGIN!],
   emailAndPassword: { enabled: false },
-  secret: env.BETTER_AUTH_SECRET,
+  secret: process.env.BETTER_AUTH_SECRET,
   socialProviders: {
     google: {
-      clientId: env.GOOGLE_CLIENT_ID!,
-      clientSecret: env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
   plugins: [
