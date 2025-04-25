a72890190bfa719daa1cd3f9f5c6ad4e0d906c87
Anurag Banerjee <anurag@axioned.com>
2025-04-26 00:07:20 +0530

    feat: go back to node-postgres

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,4 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql, schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
