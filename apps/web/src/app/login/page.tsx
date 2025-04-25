cd908636dfd5f86e06c36552867d0c1441a3b198
Anurag Banerjee <anurag@axioned.com>
2025-04-25 23:41:54 +0530

    feat: redirect already logged in user to dashboard

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import { LoginForm } from "@/components/auth/login-form";
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
+  const { data: session, isPending } = authClient.useSession();
+
+  if (!isPending && session) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
