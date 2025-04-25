24c73e97b30d34179cf6e076fa65d1ad6116c04b
Anurag Banerjee <anurag@axioned.com>
2025-04-25 20:42:40 +0530

    feat: use node-postgres with drivers

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,4 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const db = drizzle(pool, { schema });
