9d21011d2368369941209a87769c44876181d392
Anurag Banerjee <anurag@axioned.com>
2025-04-27 16:54:27 +0530

    feat: add where condition for current active org

--- apps/server/src/routers/client.ts
+++ apps/server/src/routers/client.ts
@@ -2,10 +2,11 @@ import { z } from "zod";
 import { router, protectedProcedure } from "../lib/trpc";
 import { client } from "../db/schema";
 import { db } from "../db";
+import { eq } from "drizzle-orm";
 
 export const clientRouter = router({
-  getAll: protectedProcedure.query(async () => {
-    return await db.select().from(client);
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    return await db.select().from(client).where(eq(client.organizationId, ctx.session.session.activeOrganizationId!));
   }),
   create: protectedProcedure.input(z.object({ name: z.string().min(1) })).mutation(async ({ input, ctx }) => {
     return await db.insert(client).values({
