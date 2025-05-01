6ba74bcf9f86954f35d302dd4f80b513ec507b4a
Anurag Banerjee <anurag@axioned.com>
2025-05-01 13:14:31 +0530

    feat: map google picture to user

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -17,6 +17,15 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      overrideUserInfoOnSignIn: true,
+      mapProfileToUser: (profile) => {
+        console.log(profile);
+        return {
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
     },
   },
   plugins: [
