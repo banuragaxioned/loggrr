bae833936f9be1e6cba10e9fdf8721d156aecd34
Anurag Banerjee <anurag@axioned.com>
2025-05-06 00:22:57 +0530

    feat: use form in magic link

--- apps/web/src/components/auth/google-button.tsx
+++ apps/web/src/components/auth/google-button.tsx
@@ -2,12 +2,13 @@
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "@/lib/auth-client";
-import { useForm } from "@tanstack/react-form";
 import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
+import { useAppForm } from "@/components/ui/form";
+import { useCallback } from "react";
 
 export function LoginGoogleButton() {
-  const form = useForm({
+  const form = useAppForm({
     onSubmit: async () => {
       try {
         await signIn.social({
@@ -20,27 +21,32 @@ export function LoginGoogleButton() {
     },
   });
 
+  const handleSubmit = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      form.handleSubmit();
+    },
+    [form],
+  );
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        void form.handleSubmit();
-      }}
-    >
-      <Button variant="outline" className="w-full" type="submit" disabled={form.state.isSubmitting}>
-        {form.state.isSubmitting ? (
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mr-2 h-4 w-4">
-            <path
-              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-              fill="currentColor"
-            />
-          </svg>
-        )}
-        Login with Google
-      </Button>
-    </form>
+    <form.AppForm>
+      <form onSubmit={handleSubmit}>
+        <Button variant="outline" className="w-full" type="submit" disabled={form.state.isSubmitting}>
+          {form.state.isSubmitting ? (
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mr-2 h-4 w-4">
+              <path
+                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                fill="currentColor"
+              />
+            </svg>
+          )}
+          Login with Google
+        </Button>
+      </form>
+    </form.AppForm>
   );
 }
